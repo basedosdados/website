31c975ac5909c959d0add4a0cc1efb800580a086
Fred Israel <fredpublico@gmail.com>
2020-11-15 17:49:41 -0300

    mudar pagina de tabelas

--- ckanext-basedosdados/ckanext/basedosdados/custom_functions.py
+++ ckanext-basedosdados/ckanext/basedosdados/custom_functions.py
@@ -6,10 +6,13 @@ def separate_bdm_and_links_resources(resources):
     return bdm, links
 
 def is_bdm(r):
-    return r['is_bdm'] or 'bd+' in r['formato'] or '(BD+)' in r['name']
+    return r.get('is_bdm') or 'bd+' in r.get('formato', '') or '(BD+)' in r['name']
 
 def make_example_select_query(resource, package):
     return f'''SELECT * FROM "basedosdados.{get_package_bdm_schema_name(package)}.{resource['name']}" LIMIT 100'''
 
 def get_package_bdm_schema_name(package):
     return stringcase.snakecase(package['name'])
+
+def get_resource_bdm_table_name(resource):
+    return resource['name']
