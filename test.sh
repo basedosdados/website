d6e85f10b68840fa21258d6882d79e00913c9e34
Fred Israel <fredpublico@gmail.com>
2020-11-30 20:32:36 -0300

    add iframe security patch

--- test.sh
+++ test.sh
@@ -3,15 +3,15 @@ cd $(git rev-parse --show-toplevel)
 
 trap 'catch $? $LINENO' ERR ; catch() { echo "Exit-code $1 occurred on line $2" ;}
 
-URL="http://localhost:5000"
+URL=${URL:-"http://localhost:5000"}
 docker-compose up -d ckan
 utils/wait-for-200 -t 30 $URL
 
 TEST_URLS='
     # dataset organization group about
     dataset/?download_type=BD+Mais dataset/?q=futebol
-    dataset/seguranca-no-estado-de-sao-paulo
-    dataset/activity/seguranca-no-estado-de-sao-paulo
+    dataset/br-sp-ssp-seguranca
+    dataset/activity/br-sp-ssp-seguranca
     dataset/changes/f0f49216-9eab-48a4-bd39-62b6b8fdc75d
 '
 
