af1108444a2aa08259616d061acbb58cb7e9165a
Fred Israel <fredpublico@gmail.com>
2021-05-24 21:44:23 -0300

    fixed array inconsistency on jsonform

--- ckanext-basedosdados/ckanext/basedosdados/validator/resource.py
+++ ckanext-basedosdados/ckanext/basedosdados/validator/resource.py
@@ -119,7 +119,12 @@ class BdmTable(Resource):
     publisher_github: Optional[Str] # Required for tier 1
     publisher_website: Optional[Str] # Required for tier 1
 
-    bdm_file_size: Union[int, None, Literal['Unavailable']] # should not be editable in form, also, check what use is Unavailable
+    bdm_file_size: Union[int, None, Literal['Unavailable', '']] # should not be editable in form, also, check what use is Unavailable
+
+    @validator('bdm_file_size')
+    def null_string_is_none(cls, value): # TODO: check why this is not working, as it is still failing when we pass a ''. Had to add '' to type signature
+        if value == '':
+            return None
 
     # TODO: implement this
     def table_id_should_be_a_valid_bigquery_identifier(cls, value):
