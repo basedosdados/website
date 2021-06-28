bfe786fe4759bb131380587362e74aa1827d4ce8
Vinicius <vncsna@gmail.com>
2021-06-28 10:41:51 -0400

    feat: add bd_get_dataset and bd_get_table

--- ckanext-basedosdados/ckanext/basedosdados/actions/get.py
+++ ckanext-basedosdados/ckanext/basedosdados/actions/get.py
@@ -2,7 +2,43 @@ import os
 
 import ckan.plugins.toolkit as toolkit
 import requests
-from ckan.logic.action.get import package_search
+from ckan.logic.action.get import package_search, resource_search
+
+
+@toolkit.side_effect_free
+def bd_get_dataset(context, data_dict):
+    """Show dataset
+
+    :param dataset_id: dataset name
+    :type dataset_id: string
+
+    :returns: dataset most relevant
+    """
+
+    dataset_id = data_dict.get("dataset_id", "")
+
+    search = package_search(context, {"name_or_id": dataset_id.replace("_", "-")})
+    data = search.get("results", [])
+
+    return data[0]
+
+
+@toolkit.side_effect_free
+def bd_get_table(context, data_dict):
+    """Show table
+
+    :param table_id: table name
+    :type table_id: string
+
+    :returns: table most relevant
+    """
+
+    table_id = data_dict.get("table_id", "")
+
+    search = resource_search(context, {"name": table_id})
+    data = search.get("results", [])
+
+    return data[0]
 
 
 @toolkit.side_effect_free
