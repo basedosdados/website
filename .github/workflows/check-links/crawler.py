e9a9e745e1efa9931e699853a63a253ca733f74d
Vinicius <vncsna@gmail.com>
2021-07-01 20:56:12 -0400

    [infra] change referrer policy

--- .github/workflows/check-links/crawler.py
+++ .github/workflows/check-links/crawler.py
@@ -15,6 +15,11 @@ class LinkSpider(CrawlSpider):
     name = "link-checker"
     report_if = set(range(400, 600))
     handle_httpstatus_list = list(report_if)
+    
+    # Send referrer headers to every URL
+    custom_settings = {
+        "REFERRER_POLICY": "unsafe-url"
+    }
 
     start_urls = [
         "https://basedosdados.org/",
