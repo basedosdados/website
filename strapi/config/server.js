a087241e6e89b691b60300f65465e7267f2f8477
Breno Gomes <brenodega28@gmail.com>
2021-07-19 06:40:45 -0300

    fix: fixing strapi

---
+++ strapi/config/server.js
@@ -0,0 +1,11 @@
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "http://localhost/strapi",
+  admin: {
+    url: "http://localhost/admin",
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "9dbed0281a16d8ce7931362257f47ac6"),
+    },
+  },
+});
