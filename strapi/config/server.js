81236d91c8c2e922de53057edbe8f86fe24dbf5e
Breno Gomes <brenodega28@gmail.com>
2021-07-19 02:58:35 -0300

    feat: reenabling strapi

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "http://" + env("HOST", "0.0.0.0") + "/strapi",
+  url: "http://localhost/strapi",
   admin: {
-    url: "http://" + env("HOST", "0.0.0.0") + "/admin",
+    url: "http://localhost/admin",
     auth: {
       secret: env("ADMIN_JWT_SECRET", "172116ea1aca728cae4e6ed966691262"),
     },
