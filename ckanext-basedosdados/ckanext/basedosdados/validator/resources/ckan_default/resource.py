1694fe5207f6324ee2a15e86c22534399a998c6f
joaoc <joao.carabetta@gmail.com>
2021-07-25 18:40:24 -0300

    only fields on models are accepted

--- ckanext-basedosdados/ckanext/basedosdados/validator/resources/ckan_default/resource.py
+++ ckanext-basedosdados/ckanext/basedosdados/validator/resources/ckan_default/resource.py
@@ -2,11 +2,12 @@ from ckanext.basedosdados.validator import BaseModel
 from typing import Optional
 from pydantic import (
     StrictStr as Str,
+    Extra
 )
 from ckanext.basedosdados.validator.available_options import IdType
 
 
-class _CkanDefaultResource(BaseModel):
+class _CkanDefaultResource(BaseModel, extra=Extra.forbid):
     id         : IdType
     name       : Str
     description: Str
