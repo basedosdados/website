00a25945e4b98e029e76beb99c810e925ccf3419
joaoc <joao.carabetta@gmail.com>
2021-07-28 22:36:22 -0300

    clean hack options to build db

--- ckanext-basedosdados/ckanext/basedosdados/validator/packages/dataset/__init__.py
+++ ckanext-basedosdados/ckanext/basedosdados/validator/packages/dataset/__init__.py
@@ -27,7 +27,7 @@ class Dataset(_CkanDefaults):
     dataset_id        : Optional[Str]                                  = DATASET_ID_FIELD
     title             : Optional[Str]                                  = TITLE_FIELD
     description       : Optional[Str]                                  = DESCRIPTION_FIELD
-    spatial_coverage  : Union[Optional[SpatialCoverage],Optional[Str]] = SPATIAL_COVERAGE_FIELD # TODO: remove Optional[Str] after migration
+    spatial_coverage  : Optional[SpatialCoverage]                      = SPATIAL_COVERAGE_FIELD 
     temporal_coverage : Optional[TemporalCoverageEnum]                 = TEMPORAL_COVERAGE_FIELD
     update_frequency  : Optional[TimeUnitEnum]                         = UPDATE_FREQUENCY_FIELD
     entity            : Optional[Set[EntityEnum]]                      = ENTITY_FIELD
