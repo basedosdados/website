ce500a01016d086557591cb717c16f9d554e2beb
d116626 <d116626@gmail.com>
2021-08-27 09:38:12 -0300

    fix import order

--- ckanext-basedosdados/ckanext/basedosdados/validator/resources/__init__.py
+++ ckanext-basedosdados/ckanext/basedosdados/validator/resources/__init__.py
@@ -1,17 +1,19 @@
 # -------------------------------------
 # RESOUCES CKAN DEFAULT
 # -------------------------------------
+from ckanext.basedosdados.validator.resources.ckan_default.resource import (
+    _CkanDefaultResource,  # this has to be the first because of import initializations
+)
+
 # -------------------------------------
 # RESOUCES BDM
 # -------------------------------------
 from ckanext.basedosdados.validator.resources.bdm.column import (
-    BdmColumns,  # this has to be the first one!
+    BdmColumns,  # this has to be the second because of import initializations
 )
 from ckanext.basedosdados.validator.resources.bdm.dictionary import BdmDictionary
 from ckanext.basedosdados.validator.resources.bdm.table import BdmTable
-from ckanext.basedosdados.validator.resources.ckan_default.resource import (
-    _CkanDefaultResource,
-)
+
 
 # -------------------------------------
 # RESOUCES EXTERNAL LINK
