e05d74650da29d8ec48253f2bfcbde9844f32f4d
Ricardo Dahis <6617207+rdahis@users.noreply.github.com>
2021-10-07 14:28:55 -0300

    Update measurement_unit.py

--- ckanext-basedosdados/ckanext/basedosdados/validator/available_options/measurement_unit.py
+++ ckanext-basedosdados/ckanext/basedosdados/validator/available_options/measurement_unit.py
@@ -1,9 +1,25 @@
 from .attr_enum import AttrEnum
 
-
 class MeasurementUnitEnum(AttrEnum):
-    # fmt: off
+    
+    # space
     km  = {"label": "km"}
     km2 = {"label": "km2"}
     m   = {"label": "m"}
-    # fmt: on
+    m2  = {"label": "m2"}
+    ha  = {"label": "ha"}
+    
+    # mass
+    kg  = {"label": "kg"}
+    t   = {"label": "t"}
+    
+    # currency
+    brl = {"label": "BRL"}
+    usd = {"label": "USD"}
+    
+    # date_time
+    year = {"label": "ano"}
+    month = {"label": "mês"}
+    day = {"label": "dia"}
+    second = {"label": "segundo"}
+    
