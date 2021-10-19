7cf6678aa433f49175aca3d41cb9ec115da0d67b
Ricardo Dahis <6617207+rdahis@users.noreply.github.com>
2021-10-19 19:21:59 -0300

    [infra] Novas unidades de medida (#123)
    * Update measurement_unit.py
    
    * more currencies
    
    * Adds minimum wage and percentage.
    
    * New units and extends keys.
    
    * Adjustments and alignment.
    
    * Square foot and inch.
    
    * Update measurement_unit.py
    
    Co-authored-by: d116626 <d116626@gmail.com>
    Co-authored-by: Fernanda Scovino <fscovinom@gmail.com>

--- ckanext-basedosdados/ckanext/basedosdados/validator/available_options/measurement_unit.py
+++ ckanext-basedosdados/ckanext/basedosdados/validator/available_options/measurement_unit.py
@@ -1,9 +1,52 @@
 from .attr_enum import AttrEnum
 
-
 class MeasurementUnitEnum(AttrEnum):
     # fmt: off
-    km  = {"label": "km"}
-    km2 = {"label": "km2"}
-    m   = {"label": "m"}
+    # space
+    kilometer   = {"label": "km"}
+    kilometer2  = {"label": "km2"}
+    meter       = {"label": "m"}
+    meter2      = {"label": "m2"}
+    centimeter  = {"label": "cm"}
+    centimeter2 = {"label": "cm2"}
+    hectare     = {"label": "hectare"}
+    acre        = {"label": "acre"}
+    mile        = {"label": "mi"}
+    mile2       = {"label": "mi2"}
+    foot        = {"label": "pé"}
+    foot2       = {"label": "pé2"}
+    inch        = {"label": "polegada"}
+    inch2       = {"label": "polegada2"}
+    
+    # mass
+    kilogram = {"label": "kg"}
+    ton      = {"label": "tonelada"}
+    ounce    = {"label": "onça"}
+    gallon   = {"label": "galão"}
+    
+    # people
+    person = {"label": "pessoa"}
+    
+    # currency
+    brl = {"label": "BRL"}
+    usd = {"label": "USD"}
+    eur = {"label": "EUR"}
+    gbp = {"label": "GBP"}
+    cny = {"label": "CNY"}
+    jpy = {"label": "JPY"}
+    
+    # economics
+    minimum_wage = {"label": "salário mínimo"}
+    
+    # date_time
+    year     = {"label": "ano"}
+    semester = {"label": "semestre"}
+    quarter  = {"label": "trimestre"}
+    month    = {"label": "mês"}
+    week     = {"label": "semana"}
+    day      = {"label": "dia"}
+    second   = {"label": "segundo"}
+    
+    # percentage
+    percent = {"label": "porcentagem"}
     # fmt: on
