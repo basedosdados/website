930f15a62399982351721873344ab1d59da53669
Ricardo Dahis <6617207+rdahis@users.noreply.github.com>
2021-11-02 11:04:49 -0300

    Update __init__.py

--- ckanext-basedosdados/ckanext/basedosdados/validator/resources/bdm/__init__.py
+++ ckanext-basedosdados/ckanext/basedosdados/validator/resources/bdm/__init__.py
@@ -12,17 +12,17 @@ from pydantic import StrictStr as Str
 
 class LastUpdated(BaseModel):
     # fmt: off
-    metadata: Optional[datetime]  = Field(title="Metadados",user_input_hint=["Última atualização: metadados"])
-    data     : Optional[datetime] = Field(title="Dados",user_input_hint=["Última atualização: dados"])
-    release  : Optional[datetime] = Field(title="Dados Originais",user_input_hint=["Último lançamento: dados originais"])
+    metadata : Optional[datetime] = Field(title="Metadados",description=["Última atualização: metadados"])
+    data     : Optional[datetime] = Field(title="Dados",description=["Última atualização: dados"])
+    release  : Optional[datetime] = Field(title="Dados Originais",description=["Último lançamento: dados originais"])
     # fmt: on
 
 
 class PublishedBy(BaseModel):
     # fmt: off
-    name        : Optional[Str] = Field(title="Nome",user_input_hint=["<nome [você]>"])
-    email       : Optional[Str] = Field(title="Email",user_input_hint=["<email>"])
-    github_user : Optional[Str] = Field(title="Usuário Github",user_input_hint=["<usuário Github>"])
-    website     : Optional[Str] = Field(title="Website",user_input_hint=["<www.exemplo.com>"])
-    ckan_user   : Optional[Str] = Field(title="Usuário CKAN",user_input_hint=["<id do usuário no ckan>"])
+    name        : Optional[Str] = Field(title="Nome",description=["<nome [você]>"])
+    email       : Optional[Str] = Field(title="Email",description=["<email>"])
+    github_user : Optional[Str] = Field(title="Usuário Github",description=["<usuário Github>"])
+    ckan_user   : Optional[Str] = Field(title="Usuário CKAN",description=["<id do usuário no ckan>"])
+    website     : Optional[Str] = Field(title="Website",description=["<www.exemplo.com>"])
     # fmt: on
