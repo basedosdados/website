00a0f30a20ec03c239ff472194853f83708673fc
Breno Gomes <brenodega28@gmail.com>
2021-11-22 20:11:57 -0300

    fix: adding catch to axios

--- next/pages/api/translations.js
+++ next/pages/api/translations.js
@@ -3,11 +3,13 @@ import axios from "axios";
 export async function getTranslations() {
   return axios
     .get("http://ckan:5000/api/3/action/bd_translation")
-    .then(({ data }) => data.result);
+    .then(({ data }) => data.result)
+    .catch(() => []);
 }
 
 export async function getAvailableOptionsTranslations() {
   return axios
     .get("http://ckan:5000/api/3/action/bd_available_options_dict")
-    .then(({ data }) => data.result);
+    .then(({ data }) => data.result)
+    .catch(() => []);
 }
