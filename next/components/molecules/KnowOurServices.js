3016bfed6c99bc5f46906f74c609b3e764ce2cc2
Breno Gomes <brenodega28@gmail.com>
2021-11-30 18:31:20 -0300

    feat: new services page

--- next/components/molecules/KnowOurServices.js
+++ next/components/molecules/KnowOurServices.js
@@ -3,31 +3,57 @@ import Image from "next/image";
 import { useState } from "react";
 import BigTitle from "../atoms/BigTitle";
 import SectionText from "../atoms/SectionText";
+import SectionTitle from "../atoms/SectionTitle";
 
 export function KnowOurServices({ ...props }) {
-  const [currentTab, setCurrentTab] = useState("Captura de dados");
+  const [currentTab, setCurrentTab] = useState("1. Demanda");
   const tabs = {
-    "Captura de dados": {
-      image:
-        "https://basedosdados-static.s3.us-east-2.amazonaws.com/images/ilustracao_captura_dados-2.png",
-      text: "Subimos e disponibilizamos dados sob demanda e com rapidez. Isso significa que você pode aproveitar todo o processamento do Google BigQuery sem precisar criar e manter uma infraestrutura própria. ",
+    "1. Demanda": {
+      title: "Identificação de demandas e necessidades do cliente",
+      text: "Trabalhamos colaborativamente para compreender as reais necessidades por trás dos pedidos de nossos clientes. O trabalho de descoberta compreende discussões sobre os objetivos e usos dos dados a serem capturados. É neste passo que fica claro para nós e nossos clientes nosso objetivo final.",
     },
-    Análise: {
-      image:
-        "https://basedosdados-static.s3.us-east-2.amazonaws.com/images/ilustracao_analises.png",
-      text: "Preparamos indicadores, consultas e cruzamentos de dados do nosso datalake na forma de tabelas privadas de fácil consulta para seu projeto ou organização.",
+    "2. Planejamento": {
+      title: "Alinhamento de expectativas de negócio e de engenharia",
+      text: "O segundo passo é entender qual a melhor forma de entregarmos os dados desejados. Explicamos nosso processo padronizado de engenharia e buscamos entregar o valor dos dados da forma mais acessível e completa possível. ",
     },
-    Mentoria: {
-      image:
-        "https://basedosdados-static.s3.us-east-2.amazonaws.com/images/ilustracao_mentoria.png",
-      text: "Ensinamos como aplicar nossa metodologia de limpeza, estruturação e padronização de dados no seu projeto ou organização através de workshops e materiais exclusivos.",
+    "3. Pesquisa de bases": {
+      title: "Identificação das bases de dados",
+      text: (
+        <>
+          O próximo passo é realizar uma varredura em bases de dados, sejam elas
+          já presentes em nosso <i>datalake</i>, catalogadas em nosso buscador
+          ou ainda não disponibilizadas de forma organizada pelo nosso time. O
+          objetivo é identificar como extrair todo valor requisitado de maneira
+          selecionada e estruturada.
+        </>
+      ),
+    },
+    "4. Orçamento": {
+      title: "Orçamento transparente e simplificado",
+      text: (
+        <>
+          Uma vez claramente alinhado o escopo, formato e expectativas,
+          entregamos uma proposta de orçamento completamente transparente.
+          Baseamos nosso trabalho na quantidade e complexidade de{" "}
+          <i>datasets</i>, trabalho necessário e urgência. Tudo explícito de
+          forma direta e alinhada com o que foi identificado até então.
+        </>
+      ),
+    },
+    "5. Execução": {
+      title: "Execução ágil e entrega de valor",
+      text: (
+        <>
+          Por fim, nossa entrega é comprometida com prazos e, sobretudo,
+          qualidade. A comunicação é constante durante toda execução.
+          Trabalhando de forma <i>lean</i> e com profissionais experientes na
+          área, nosso time é focado em satisfazer nossos clientes.
+        </>
+      ),
     },
   };
   return (
-    <VStack {...props} width="100%">
-      <BigTitle textAlign="center" maxWidth="100%" paddingBottom="40px">
-        Conheça nossos serviços
-      </BigTitle>
+    <VStack {...props} width={{ base: "100%", lg: "90%" }}>
       <HStack
         boxShadow="0px 2px 5px 1px rgba(0, 0, 0, 0.25)"
         borderRadius="15px"
@@ -36,6 +62,8 @@ export function KnowOurServices({ ...props }) {
         position="relative"
         backgroundColor="white"
         padding="0px 5%"
+        overflowX={{ base: "scroll", lg: "none" }}
+        maxW={{ base: "300px", lg: "initial" }}
       >
         {Object.keys(tabs).map((tabName) => (
           <Heading
@@ -44,6 +72,7 @@ export function KnowOurServices({ ...props }) {
             fontSize="18px"
             height="50px"
             alignItems="center"
+            whiteSpace="nowrap"
             display="flex"
             boxSizing="border-box"
             letterSpacing="0.03rem"
@@ -59,28 +88,19 @@ export function KnowOurServices({ ...props }) {
           </Heading>
         ))}
       </HStack>
-      <Stack
-        width={{ base: "95%", lg: "70%" }}
-        maxW="850px"
-        backgroundColor="#FAFAFA"
-        padding="70px 50px"
-        paddingBottom="50px"
-        paddingLeft={{ base: "50px", lg: "0px" }}
+      <VStack
+        width={{ base: "95%", lg: "100%" }}
+        border="2px solid #E3E3E3"
+        padding="70px"
         borderRadius="22.3575px"
-        transform="translateY(-20px)"
-        direction={{ base: "column", lg: "row" }}
+        transform="translateY(-35px)"
+        align="flex-start"
       >
-        <Box minH="200px" flex="1" position="relative">
-          <Image
-            src={tabs[currentTab].image}
-            objectFit="contain"
-            layout="fill"
-          />
-        </Box>
+        <SectionTitle fontSize="20px">{tabs[currentTab].title}</SectionTitle>
         <SectionText fontWeight="400" flex="1" lineHeight="30px">
           {tabs[currentTab].text}
         </SectionText>
-      </Stack>
+      </VStack>
     </VStack>
   );
 }
--- next/pages/servicos.js
+++ next/pages/servicos.js
@@ -1,9 +1,10 @@
 import { Image } from "@chakra-ui/image";
-import { VStack } from "@chakra-ui/layout";
+import { Box, Flex, HStack, Stack, VStack } from "@chakra-ui/layout";
 import BigTitle from "../components/atoms/BigTitle";
 import Link from "../components/atoms/Link";
 import RoundedButton from "../components/atoms/RoundedButton";
 import SectionText from "../components/atoms/SectionText";
+import SectionTitle from "../components/atoms/SectionTitle";
 import { KnowOurServices } from "../components/molecules/KnowOurServices";
 import { NamedAvatar } from "../components/molecules/NamedAvatar";
 import { MainPageTemplate } from "../components/templates/main";
@@ -13,41 +14,235 @@ export async function getStaticProps(context) {
   return await withStrapiPages();
 }
 
+function FixedBottomBar() {
+  return (
+    <Stack
+      position="fixed"
+      bottom="0px"
+      width="100%"
+      padding="10px 20px"
+      height="70px"
+      backgroundColor="#7EC876"
+      justify="center"
+      align="center"
+      spacing={10}
+      boxShadow="0px 2px 10px 2px rgba(0, 0, 0, 0.15)"
+      zIndex="100"
+      direction={{ base: "row", lg: "column" }}
+    >
+      <SectionText fontWeight="400" fontFamily="Ubuntu" color="white">
+        Gostaria de marcar uma reunião com nossa equipe?
+      </SectionText>
+      <Link
+        position={{ base: "initial", lg: "fixed" }}
+        bottom="14px"
+        right="20%"
+        href="/blog/1/"
+      >
+        <RoundedButton>Entre em contato</RoundedButton>
+      </Link>
+    </Stack>
+  );
+}
+
+function Section({
+  title,
+  children,
+  listTitle = "",
+  imageUrl,
+  listChildren = [],
+  listBulletFunc = () => (
+    <Image
+      height="30px"
+      width="30px"
+      src="https://basedosdados-static.s3.us-east-2.amazonaws.com/images/Group+122.png"
+    />
+  ),
+  ...props
+}) {
+  return (
+    <Flex direction={{ base: "column-reverse", lg: "row" }} {...props}>
+      <VStack align="flex-start" flex="3">
+        <SectionTitle fontSize="24px" fontFamily="Ubuntu" marginBottom="20px">
+          {title}
+        </SectionTitle>
+        <SectionText fontSize="15px" paddingBottom="20px">
+          {children}
+        </SectionText>
+        <VStack align="flex-start">
+          <SectionText fontSize="15px">{listTitle}</SectionText>
+          {listChildren.map((children, index) => (
+            <HStack>
+              {listBulletFunc(index)}
+              <SectionText fontSize="15px">{children}</SectionText>
+            </HStack>
+          ))}
+        </VStack>
+      </VStack>
+      <Box
+        paddingBottom={{ base: "30px", lg: "0px" }}
+        marginLeft="5vw"
+        flex="2"
+      >
+        <Image src={imageUrl} />
+      </Box>
+    </Flex>
+  );
+}
+
+function BorderBox({ title, children }) {
+  return (
+    <VStack
+      textAlign="center"
+      borderRadius="18px"
+      padding="25px"
+      border="1.5px solid #DEDFE0"
+      width="350px"
+      spacing={4}
+    >
+      <SectionTitle fontSize="20px">{title}</SectionTitle>
+      <SectionText fontWeight="400" fontSize="16px" textAlign="center">
+        {children}
+      </SectionText>
+    </VStack>
+  );
+}
+
 export default function Services({ strapiPages }) {
   return (
     <MainPageTemplate strapiPages={strapiPages}>
-      <VStack width="100%">
-        <KnowOurServices paddingBottom="50px" />
-        <Link href="/blog/1/">
-          <RoundedButton height="40px" fontSize="14px" minWidth="190px">
-            Entre em contato
-          </RoundedButton>
-        </Link>
-        <BigTitle paddingTop="50px" textAlign="center" paddingBottom="20px">
-          Depoimentos
+      <VStack
+        paddingTop={{ base: "50px", lg: "0px" }}
+        width="80%"
+        margin="auto"
+      >
+        <BigTitle textAlign="center" maxWidth="100%" paddingBottom="20px">
+          Conheça nossos serviços
         </BigTitle>
-        <VStack width="50%" maxW="600px">
-          <Image
-            width="60px"
-            paddingBottom="15px"
-            alignSelf="flex-start"
-            src="https://basedosdados-static.s3.us-east-2.amazonaws.com/images/%E2%80%9C.png"
-          />
-          <SectionText lineHeight="35px">
-            Com o apoio da Base dos Dados realizamos o levantamento das
-            desigualdades de gênero e raça nas eleições 2016-2020 e das
-            distorções de financiamento de campanhas de prefeitas e vereadoras
-            nas últimas eleições, o que resultará no lançamento de uma
-            publicação inédita.
-          </SectionText>
-          <NamedAvatar
-            paddingBottom="50px"
-            alignSelf="flex-end"
-            name="Marina Barros"
-            position="Instituto Alziras"
-          />
-        </VStack>
+        <SectionText fontFamily="Ubuntu" paddingBottom="40px">
+          A Base dos Dados captura, trata e analisa os dados que você ou sua
+          organização precisa.
+        </SectionText>
+        <Section
+          title="Captura de dados"
+          imageUrl="https://basedosdados-static.s3.us-east-2.amazonaws.com/images/ilustracao_captura_dados-2.png"
+          listTitle="Principais vantagens"
+          paddingBottom="50px"
+          listChildren={[
+            <>
+              <b>Rapidez:</b> Mesmo <i>queries</i> muito complexas demoram
+              apenas minutos para serem processadas;
+            </>,
+            <>
+              <b>Escala</b>: Nosso <i>datalake</i> escala magicamente para
+              hexabytes se necessário;
+            </>,
+            <>
+              <b>Baixo custo</b>: Todo usuário possui 1TB gratuito por mês para
+              consulta aos dados.
+            </>,
+          ]}
+        >
+          <b>
+            Capturamos e disponibilizamos dados sob demanda com rapidez, escala
+            e baixo custo.
+          </b>{" "}
+          Você não precisa se preocupar em criar e manter uma infraestrutura
+          própria ou escrever códigos - nós cuidamos disso. Tudo é feito
+          seguindo a metodologia de tratamento e padronização da Base dos Dados,
+          e conforme as melhores práticas de engenharia de dados do mercado. As
+          tabelas tratadas podem ser disponibilizadas com exclusividade ou serem
+          públicas.
+        </Section>
+        <KnowOurServices paddingBottom="50px" />
+        <Section
+          title="Análise de dados"
+          imageUrl="https://basedosdados-static.s3.us-east-2.amazonaws.com/images/ilustracao_analises.png"
+          listTitle="Exemplos de perguntas que podemos responder:"
+          paddingBottom="50px"
+          listChildren={[
+            <>
+              Como vem evoluindo a economia brasileira, com indicadores
+              detalhados mensais por setor?
+            </>,
+            <>
+              Quais escolas terão <i>performance</i> acima da média do estado em
+              matemática no ano que vem?
+            </>,
+            <>
+              Quantas vezes o Diário Oficial da União publica sobre certo tema a
+              cada dia?
+            </>,
+          ]}
+        >
+          <b>
+            Construímos análises, relatórios e indicadores essenciais para sua
+            pesquisa ou tomada de decisão.
+          </b>{" "}
+          As informações e insights são geradas a partir de diversos conjuntos
+          de dados já tratados no nosso datalake público atrelado à expertise da
+          nossa equipe de Dados. E mais: utilizamos nossa metodologia padrão de
+          tratamento para que você também possa cruzar esses dados com quaisquer
+          outras bases disponíveis no datalake.
+        </Section>
+        <Flex paddingBottom="50px" direction={{ base: "column", lg: "row" }}>
+          <Box flex="1">
+            <Image src="https://basedosdados-static.s3.us-east-2.amazonaws.com/images/grafico_combustiveis_inflacao.png" />
+          </Box>
+          <Box flex="1">
+            <Image src="https://basedosdados-static.s3.us-east-2.amazonaws.com/images/grafico_desligamentos_morte.png" />
+          </Box>
+        </Flex>
+        <Section
+          title="Consultoria de dados"
+          imageUrl="https://basedosdados-static.s3.us-east-2.amazonaws.com/images/ilustracao_mentoria.png"
+          listTitle="Principais vantagens:"
+          paddingBottom="50px"
+          listChildren={[
+            <>
+              Experiência em construir infraestruturas capazes de suportar
+              milhares de acessos mensais;
+            </>,
+            <>Flexibilidade para projetos remotos em qualquer lugar do país;</>,
+            <>
+              Equipe diversa e com expertise em diferentes áreas do conhecimento
+              e dados.
+            </>,
+          ]}
+        >
+          <b>
+            Orientamos como aplicar a nossa metodologia de limpeza, estruturação
+            e padronização de dados
+          </b>{" "}
+          no seu projeto ou organização através de workshops e materiais
+          exclusivos. Reproduzir nosso processo de tratamento em seu próprio
+          banco de dados pode poupar horas de trabalho de sua equipe ao
+          consultar, manipular ou atualizar as informações.
+        </Section>
+        <Stack
+          justifyContent="space-between"
+          width="100%"
+          direction={{ base: "column", lg: "row" }}
+          align="center"
+        >
+          <BorderBox title="Infraestrutura">
+            Mentoria para equipes de engenharia de dados que buscam estruturar
+            processos de manutenção de dados, seus próprios datalakes ou ainda
+            alavancarem-se utilizando a infraestrutura da Base dos Dados.
+          </BorderBox>
+          <BorderBox title="Análise">
+            Workshops práticos, mostrando como funciona o datalake da Base dos
+            Dados e ensinando como explorar dados públicos para matérias
+            jornalísticas ou pesquisas científicas.
+          </BorderBox>
+          <BorderBox title="Programação">
+            Programas personalizados de ensino para utilizar pacotes de Python e
+            R da Base dos Dados, além de mentoria de SQL para construir análises
+            rápidas e escaláveis direto na nuvem.
+          </BorderBox>
+        </Stack>
       </VStack>
+      <FixedBottomBar />
     </MainPageTemplate>
   );
 }
