6c06c22ea9c136e8d9483693ec99aa50ccc81fa7
Ricardo Dahis <6617207+rdahis@users.noreply.github.com>
2021-12-19 12:05:12 -0300

    Update directory.py

--- ckanext-basedosdados/ckanext/basedosdados/validator/available_options/directory.py
+++ ckanext-basedosdados/ckanext/basedosdados/validator/available_options/directory.py
@@ -5,4 +5,5 @@ class DirectoryEnum(AttrEnum):
     # fmt: off
     br_bd_diretorios_brasil     = {"label": "br_bd_diretorios_brasil"}
     br_bd_diretorios_data_tempo = {"label": "br_bd_diretorios_data_tempo"}
+    br_bd_diretorios_mundo      = {"label": "br_bd_diretorios_mundo"}
     # fmt: on
