31af6f140200e45bfda2170de2cc3216471ee0bb
Ricardo Dahis <6617207+rdahis@users.noreply.github.com>
2021-12-21 10:39:51 -0300

    Update directory.py

--- ckanext-basedosdados/ckanext/basedosdados/validator/available_options/directory.py
+++ ckanext-basedosdados/ckanext/basedosdados/validator/available_options/directory.py
@@ -3,7 +3,28 @@ from .attr_enum import AttrEnum
 
 class DirectoryEnum(AttrEnum):
     # fmt: off
+    br_bd_diretorios_africa_sul = {"label": "br_bd_diretorios_africa_sul"}
+    br_bd_diretorios_alemanha   = {"label": "br_bd_diretorios_alemanha"}
+    br_bd_diretorios_argentina  = {"label": "br_bd_diretorios_argentina"}
+    br_bd_diretorios_australia  = {"label": "br_bd_diretorios_australia"}
+    br_bd_diretorios_bolivia    = {"label": "br_bd_diretorios_bolivia"}
     br_bd_diretorios_brasil     = {"label": "br_bd_diretorios_brasil"}
+    br_bd_diretorios_canada     = {"label": "br_bd_diretorios_canada"}
+    br_bd_diretorios_chile      = {"label": "br_bd_diretorios_chile"}
+    br_bd_diretorios_china      = {"label": "br_bd_diretorios_china"}
+    br_bd_diretorios_colombia   = {"label": "br_bd_diretorios_colombia"}
     br_bd_diretorios_data_tempo = {"label": "br_bd_diretorios_data_tempo"}
+    br_bd_diretorios_dinamarca  = {"label": "br_bd_diretorios_dinamarca"}
+    br_bd_diretorios_espanha    = {"label": "br_bd_diretorios_espanha"}
+    br_bd_diretorios_franca     = {"label": "br_bd_diretorios_franca"}
+    br_bd_diretorios_india      = {"label": "br_bd_diretorios_india"}
+    br_bd_diretorios_inglaterra = {"label": "br_bd_diretorios_inglaterra"}
+    br_bd_diretorios_eua        = {"label": "br_bd_diretorios_eua"}
+    br_bd_diretorios_mexico     = {"label": "br_bd_diretorios_mexico"}
     br_bd_diretorios_mundo      = {"label": "br_bd_diretorios_mundo"}
+    br_bd_diretorios_noruega    = {"label": "br_bd_diretorios_noruega"}
+    br_bd_diretorios_peru       = {"label": "br_bd_diretorios_peru"}
+    br_bd_diretorios_portugal   = {"label": "br_bd_diretorios_portugal"}
+    br_bd_diretorios_suecia     = {"label": "br_bd_diretorios_suecia"}
+    br_bd_diretorios_suica      = {"label": "br_bd_diretorios_suica"}
     # fmt: on
