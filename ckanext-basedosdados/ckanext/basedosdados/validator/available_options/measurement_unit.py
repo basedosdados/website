9c569e9542b6ceff820cc10413ab1295444838b2
Mauricio Fagundes <mauricio.fagundes@gmail.com>
2022-01-03 14:49:19 -0300

    Measurement additions (#192)
    * Adding and reorganizing measurement units
    
    * Splitting linear distances and areas
    
    * reorganizes keys and values
    
    Co-authored-by: Ricardo Dahis <6617207+rdahis@users.noreply.github.com>

--- ckanext-basedosdados/ckanext/basedosdados/validator/available_options/measurement_unit.py
+++ ckanext-basedosdados/ckanext/basedosdados/validator/available_options/measurement_unit.py
@@ -3,27 +3,49 @@ from .attr_enum import AttrEnum
 
 class MeasurementUnitEnum(AttrEnum):
     # fmt: off
-    # space
-    km          = {"label": "km"}
-    km2         = {"label": "km2"}
-    m           = {"label": "m"}
-    meter2      = {"label": "m2"}
-    centimeter  = {"label": "cm"}
-    centimeter2 = {"label": "cm2"}
-    hectare     = {"label": "hectare"}
-    acre        = {"label": "acre"}
-    mile        = {"label": "mi"}
-    mile2       = {"label": "mi2"}
-    foot        = {"label": "pé"}
-    foot2       = {"label": "pé2"}
-    inch        = {"label": "polegada"}
-    inch2       = {"label": "polegada2"}
     
+    # distance
+    kilometer   = {"label": "km"}
+    meter       = {"label": "m"}
+    centimeter  = {"label": "cm"}
+    mile        = {"label": "mi"}
+    foot        = {"label": "pé"}
+    inch        = {"label": "polegada"}
+
+    # area
+    kilometer2  = {"label": "km2"}
+    meter2      = {"label": "m2"}
+    centimeter2 = {"label": "cm2"}
+    hectare     = {"label": "ha"}
+    acre        = {"label": "acre"}
+    mile2       = {"label": "mi2"}
+    foot2       = {"label": "pé2"}
+    inch2       = {"label": "polegada2"}
+
     # mass
-    kilogram = {"label": "kg"}
     ton      = {"label": "tonelada"}
+    kilogram = {"label": "kg"}
+    gram     = {"label": "g"}
+    miligram = {"label": "mg"}
     ounce    = {"label": "onça"}
+
+    # volume
     gallon   = {"label": "galão"}
+    litre    = {"label": "litro"}
+    militre  = {"label": "ml"}
+    meter3   = {"label": "m3"}
+    mile3    = {"label": "mi3"}
+    foot3    = {"label": "pé3"}
+    inch3    = {"label": "polegada3"}
+    barrel   = {"label": "barril"}
+    boe      = {"label": "barril de óleo equivalente"}
+    toe      = {"label": "tonelada de óleo equivalente"}
+
+    # energy
+    watt           = {"label": "W"}
+    kilowatt       = {"label": "kW"}
+    volt           = {"label": "V"}
+    kilovolt       = {"label": "kV"}
     
     # people
     person    = {"label": "pessoa"}
@@ -40,7 +62,7 @@ class MeasurementUnitEnum(AttrEnum):
     # economics
     minimum_wage = {"label": "salário mínimo"}
     
-    # date_time
+    # date-time
     year     = {"label": "ano"}
     semester = {"label": "semestre"}
     quarter  = {"label": "trimestre"}
