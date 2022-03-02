54379382c0c4ab402c2c0c37671e5ef962663674
Aldemir <aldemirlucas23@gmail.com>
2022-03-02 15:57:19 -0300

    ajust in datasets/Pedidos Lai

--- next/components/organisms/InformationRequestPage.js
+++ next/components/organisms/InformationRequestPage.js
@@ -1,4 +1,10 @@
-import { VStack, Image } from "@chakra-ui/react";
+import { 
+  VStack,
+  HStack,
+  Image,
+  Box, 
+  Text,
+} from "@chakra-ui/react";
 import Title from "../atoms/Title";
 import { ExpandableTable } from "../molecules/ExpandableTable";
 import {
@@ -10,6 +16,7 @@ import { BaseResourcePage } from "../molecules/BaseResourcePage";
 import { deleteResource, updateResource } from "../../pages/api/datasets";
 import { getInformationRequestSchema } from "../../pages/api/schemas";
 import { SchemaForm } from "../molecules/SchemaForm";
+import Link from "../atoms/Link";
 
 export function InformationRequestPage({
   translations,
@@ -41,8 +48,53 @@ export function InformationRequestPage({
       buttonRightIcon={<Image src="/img/icons/white_right_arrow.svg" />}
       onClick={() => window.open(resource.url)}
     >
-      <VStack width="100%" spacing={3} alignItems="flex-start">
-        <Title>Metadados do pedido LAI</Title>
+      <VStack width="100%" letterSpacing="0.5px" spacing={3} alignItems="flex-start">
+
+        <VStack
+          alignItems="flex-start"
+          padding="15px 20px"
+          backgroundColor="#ECF7FF"
+          borderRadius="10px"
+          color="#252A32"
+          fontFamily="Lato"
+          fontSize="16px"
+        >
+          <Text>
+            A disponibilização destes dados é resultado de uma parceria com:
+          </Text>
+          <HStack margin="10px 0" gridGap={2}>
+            <Image 
+              border="1px solid #DEDFE0"
+              borderRadius="10px" 
+              src={"/img/icons/fiquem_sabendo.png"}
+              width="80px"
+              height="80px"
+            />
+            <Box>
+              <Text marginBottom="5px" fontWeight="bold">
+                Fiquem Sabendo
+              </Text>
+              <Text>
+                Agência de dados públicos independente e especializada na Lei de Acesso à Informação.
+              </Text>
+            </Box>
+          </HStack>
+        </VStack>
+
+        <VStack
+          alignItems="flex-start"
+          marginTop="30px !important"
+          fontFamily="Lato"
+          fontSize="16px"
+        >
+          <Title fontWeigth="normal">Detalhes do pedido</Title>
+          <Text >
+            Você pode consultar o <Link isExternal={true} url={resource.url} color="#42B0FF"> pedido </Link> para saber mais detalhes. 
+          </Text>
+        </VStack>
+
+
+        <Title marginTop="30px !important" fontWeigth="normal">Metadados do pedido</Title>
         <ExpandableTable
           headers={["nome", "valor"]}
           values={formatObjectsInArray(
@@ -50,21 +102,11 @@ export function InformationRequestPage({
               translations,
               availableOptionsTranslations,
               filterOnlyValidValues(resource, [
-                "title",
                 "url",
-                "description",
-                "language",
-                "has_structured_data",
-                "has_api",
-                "is_free",
-                "requires_registration",
-                "availability",
-                "country_ip_address_required",
-                "license",
+                "requested_by",
                 "spatial_coverage",
-                "temporal_coverage",
-                "update_frequency",
-                "observation_level",
+                "data_url",
+                "state",
               ])
             )
           )}
