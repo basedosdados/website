c6bd7acf8512dd71b751cf83683b668ace6903ef
Aldemir Lucas <82185307+AldemirLucas@users.noreply.github.com>
2022-03-09 18:19:42 -0300

    Update ThemeCatalog.js

--- next/components/organisms/ThemeCatalog.js
+++ next/components/organisms/ThemeCatalog.js
@@ -159,8 +159,8 @@ export default function ThemeCatalog ({ recentDatalakeDatasets }) {
   const isMobile = useCheckMobile();
 
   useEffect(() => {
-    getGroupList().then(data => {
-      setListThemes(data.result)
+    getGroupList().then(res => {
+      setListThemes(res.data.result)
     })
     recentDatalakeDatasets ? setRecentThemes(recentDatalakeDatasets) : setRecentThemes()
   },[])
