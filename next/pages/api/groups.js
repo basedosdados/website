fc61ce404334e51d4a185ae0964146a2ca56b4db
Aldemir Lucas <82185307+AldemirLucas@users.noreply.github.com>
2022-03-09 19:05:23 -0300

    Update groups.js

--- next/pages/api/groups.js
+++ next/pages/api/groups.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export function getGroupList() {
-  return axios.get("http://ckan:5000/api/3/action/group_list?all_fields=true");
+  return axios.get("http://basedosdados.org/api/3/action/group_list?all_fields=true");
 }
