141008067f91f187f033563a3817627e8f085311
Aldemir Lucas <82185307+AldemirLucas@users.noreply.github.com>
2022-03-09 15:51:55 -0300

    theme catalog test
    Test para conferir se o catalogo de temas esta corrigido

--- next/components/organisms/ThemeCatalog.js
+++ next/components/organisms/ThemeCatalog.js
@@ -177,11 +177,11 @@ export default function ThemeCatalog ({ recentDatalakeDatasets }) {
       alignItems="center"
       gap="50px"
     >
-      {/* <Themes
-        listThemes={listThemes}
-        newRecentDataLakeDataSets={newRecentDataLake}
-        isMobileMod={isMobile} 
-      /> */}
+       <Themes
+         listThemes={listThemes}
+         newRecentDataLakeDataSets={newRecentDataLake}
+         isMobileMod={isMobile} 
+       />
 
       <CardThemes 
         isMobileMod={isMobile} 
