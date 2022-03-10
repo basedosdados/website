ad3225be18b345b043b22982683b48f6277766ce
Nayara <nayaramoraesdacosta@gmail.com>
2022-03-10 20:32:13 -0300

    corrige depoimentos

--- next/components/organisms/BePartner.js
+++ next/components/organisms/BePartner.js
@@ -9,6 +9,7 @@ import SectionTitle from "../atoms/SectionTitle";
 import { ShadowBox } from "../atoms/ShadowBox";
 import { NamedAvatar } from "../molecules/NamedAvatar";
 import { slidesToShowPlugin } from "@brainhubeu/react-carousel";
+import { useCheckMobile } from "../../hooks/useCheckMobile.hook";
 
 const Carousel = dynamic(() => import("@brainhubeu/react-carousel"), {
   ssr: false,
@@ -19,18 +20,20 @@ function Testimonial({ children, name, position, src }) {
     <VStack
       padding={{ base: "0px", lg: "30px" }}
       paddingTop={{ base: "0px", lg: "50px" }}
-      width="100%"
+      width="80%"
     >
       <Image
-        width="60px"
-        paddingBottom="15px"
+        width="50px"
+        paddingBottom="5px"
         alignSelf="flex-start"
         src="https://basedosdados-static.s3.us-east-2.amazonaws.com/images/%E2%80%9C.png"
       />
       <SectionText
         paddingLeft="50px"
         paddingBottom="20px"
-        textAlign="justify"
+        fontFamily="Ubuntu"
+        fontSize="22px"
+        textAlign="center"
         lineHeight="30px"
       >
         {children}
@@ -62,6 +65,8 @@ function PartnerBox({ src, ...props }) {
 }
 
 export function BePartner() {
+  const isMobile = useCheckMobile();
+
   return (
     <VStack width="80%" spacing={7} margin="auto">
       <BigTitle>Como impactamos pessoas e organizações</BigTitle>
@@ -126,72 +131,104 @@ export function BePartner() {
           </Stack>
         </VStack>
       </Stack>
-      <HStack w="110%">
-        <Carousel
-          plugins={[
-            "infinite",
-            "arrows",
-            "fastSwipe",
-            {
-              resolve: slidesToShowPlugin,
-              options: {
-                numberOfSlides: 1,
+      {!isMobile &&
+        <HStack w="110%">
+          <Carousel
+            plugins={[
+              "infinite",
+              "arrows",
+              "fastSwipe",
+              {
+                resolve: slidesToShowPlugin,
+                options: {
+                  numberOfSlides: 1,
+                },
               },
-            },
-          ]}
-        >
-          <Testimonial
-            name="Marina Barros"
-            position="Diretora-Executiva do Instituto Alziras"
-            src="https://basedosdados-static.s3.us-east-2.amazonaws.com/logos/alziras.png"
+            ]}
           >
-            Com o apoio da Base dos Dados realizamos o{" "}
-            <b>
-              levantamento das desigualdades de gênero e raça nas eleições
-              2016-2020
-            </b>{" "}
-            e das{" "}
-            <b>
-              distorções de financiamento de campanhas de prefeitas e vereadoras
-            </b>{" "}
-            nas últimas eleições, o que resultará no lançamento de uma
-            publicação inédita.
-          </Testimonial>
-          <Testimonial
-            name="Amanda de Albuquerque e Mariana Carvalho"
-            position="Co-fundadoras da A Ponte"
-            src="https://basedosdados-static.s3.us-east-2.amazonaws.com/logos/aponte.png"
-          >
-            Criamos um importante produto nosso, a{" "}
-            <b>Fotografia do Município</b>, em parceria com a BD. A Fotografia
-            agrega diversos indicadores sociodeconômicos dos municípios
-            brasileiros. A Ponte fez o planejamento e desenho do relatório,
-            enquanto a BD cuidou da parte técnica de visualização e gestão de
-            dados.{" "}
-            <b>
-              A parceria foi o que nos permitiu avançar rápido no
-              desenvolvimento da ferramenta, e garantiu a qualidade de dados
-              alta como queríamos desde o início.
-            </b>
-          </Testimonial>
-          <Testimonial
-            name="Fernando Barbalho"
-            position="Cientista de Dados do Tesouro Nacional"
-            src="https://basedosdados-static.s3.us-east-2.amazonaws.com/logos/tesouro_nacional.png"
-          >
-            A{" "}
-            <b>
-              siconfiBD é um pacote que traz de forma rápida e programática os
-              dados da Secretaria do Tesouro Nacional
-            </b>
-            . O código é construído de forma open-source pela equipe do Tesouro
-            e <b>utiliza as tabelas tratadas BD+ para compor funções em R</b>.
-            Qual a despesa de pessoal dos 100 municípios brasileiros mais
-            populosos? Essas e mais de outras 18 perguntas podem ser consultadas
-            diretamente pelo pacote, sem necessidade de SQL.
-          </Testimonial>
-        </Carousel>
-      </HStack>
+            <Testimonial
+              name="Marina Barros"
+              position="Diretora-Executiva do Instituto Alziras"
+              src="https://basedosdados-static.s3.us-east-2.amazonaws.com/logos/alziras.png"
+            >
+              Com o apoio da Base dos Dados realizamos o{" "}
+              <SectionText 
+                fontFamily="Ubuntu" 
+                fontSize="22px" 
+                fontWeight="500" 
+                textAlign="center"
+                lineHeight="30px"
+              >
+                levantamento das desigualdades de gênero e raça nas eleições 2016-2020 e das distorções de financiamento de campanhas de prefeitas e vereadoras
+              </SectionText>
+              nas últimas eleições, o que resultará no lançamento de uma
+              publicação inédita.
+            </Testimonial>
+            <Testimonial
+              name="Amanda de Albuquerque e Mariana Carvalho"
+              position="Co-fundadoras dA Ponte"
+              src="https://basedosdados-static.s3.us-east-2.amazonaws.com/logos/aponte.png"
+            >
+              Criamos um importante produto nosso, a{" "}
+              <SectionText 
+                fontFamily="Ubuntu" 
+                fontSize="22px" 
+                fontWeight="500" 
+                textAlign="center"
+                lineHeight="30px"
+              >
+                Fotografia do Município,{" "}
+              </SectionText>
+              em parceria com a BD. A Fotografia
+              agrega diversos indicadores socioeconômicos dos municípios
+              brasileiros. A Ponte fez o planejamento e desenho do relatório,
+              enquanto a BD cuidou da parte técnica de visualização e gestão de
+              dados.{" "}
+              <SectionText 
+                fontFamily="Ubuntu" 
+                fontSize="22px" 
+                fontWeight="500" 
+                textAlign="center"
+                lineHeight="30px"
+              >
+                A parceria foi o que nos permitiu avançar rápido no
+                desenvolvimento da ferramenta, e garantiu a qualidade de dados
+                alta como queríamos desde o início.
+              </SectionText>
+            </Testimonial>
+            <Testimonial
+              name="Fernando Barbalho"
+              position="Cientista de Dados do Tesouro Nacional"
+              src="https://basedosdados-static.s3.us-east-2.amazonaws.com/logos/tesouro_nacional.png"
+            >
+              <SectionText 
+                fontFamily="Ubuntu" 
+                fontSize="22px" 
+                fontWeight="500" 
+                textAlign="center"
+                lineHeight="30px"
+              >
+                A siconfiBD é um pacote que traz de forma rápida e programática os
+                dados da Secretaria do Tesouro Nacional.
+              </SectionText>
+              O código é construído de forma <i>open-source</i> pela equipe do Tesouro
+              e 
+              <SectionText 
+                fontFamily="Ubuntu" 
+                fontSize="22px" 
+                fontWeight="500" 
+                textAlign="center"
+                lineHeight="30px"
+              >
+                utiliza as tabelas tratadas BD+ para compor funções em R.
+              </SectionText>
+              Qual a despesa de pessoal dos 100 municípios brasileiros mais
+              populosos? Essas e mais de outras 18 perguntas podem ser consultadas
+              diretamente pelo pacote, sem necessidade de SQL.
+            </Testimonial>
+          </Carousel>
+        </HStack>
+      }
     </VStack>
   );
 }
