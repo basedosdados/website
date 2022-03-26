e7d16da587c1feb8efb418102d9b846f752a0927
nayaramrsc <nayaramoraes@basedosdados.org>
2022-03-26 18:51:22 -0300

    corrige logo do depoimento

--- next/components/molecules/NamedAvatar.js
+++ next/components/molecules/NamedAvatar.js
@@ -4,12 +4,16 @@ import SectionText from "../atoms/SectionText";
 
 export function NamedAvatar({ src, name, position, ...props }) {
   return (
-    <HStack spacing={5} {...props}>
+    <HStack spacing={3} {...props}>
       <VStack spacing={0} align="flex-end">
         <Text>{name}</Text>
         <SectionText letterSpacing="0.05rem">{position}</SectionText>
       </VStack>
-      <Avatar backgroundColor="white" src={src} />
+      <Avatar 
+        backgroundColor="white"
+        height="70px"
+        src={src} 
+      />
     </HStack>
   );
 }
