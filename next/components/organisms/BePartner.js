de6d0fb2c7336d1a1f81ecdfa6c78373ec4b9210
nayaramrsc <nayaramoraes@basedosdados.org>
2022-03-29 15:29:28 -0300

    ajusta depoimento

--- next/components/organisms/BePartner.js
+++ next/components/organisms/BePartner.js
@@ -3,6 +3,7 @@ import { HStack, Stack, VStack, Text, Center } from "@chakra-ui/layout";
 import dynamic from "next/dynamic";
 import BigTitle from "../atoms/BigTitle";
 import SectionText from "../atoms/SectionText";
+import Title from "../atoms/Title";
 import { NamedAvatar } from "../molecules/NamedAvatar";
 import { slidesToShowPlugin } from "@brainhubeu/react-carousel";
 import { useCheckMobile } from "../../hooks/useCheckMobile.hook";
@@ -15,7 +16,7 @@ function Testimonial({ children, name, position, src }) {
   return (
     <VStack
       padding={{ base: "0px", lg: "30px" }}
-      paddingTop={{ base: "0px", lg: "50px" }}
+      paddingTop={{ base: "0px", lg: "80px" }}
       width="80%"
     >
       <Stack 
@@ -28,12 +29,12 @@ function Testimonial({ children, name, position, src }) {
         />
       </Stack>
       <SectionText
-        paddingLeft="50px"
-        paddingBottom="20px"
+        padding="0px 20px 50px 20px"
         fontFamily="Ubuntu"
-        fontSize="22px"
+        fontSize="28px"
+        fontWeight="300"
         textAlign="center"
-        lineHeight="30px"
+        lineHeight="45px"
       >
         {children}
       </SectionText>
@@ -86,13 +87,11 @@ export function BePartner() {
         spacing={10}
       >
         <Center width="100%">
-          <Text
+          <Title
             zIndex={2}
-            fontSize="18px"
             fontWeight="300"
-            color="#7D7D7D"
-            fontFamily="Ubuntu"
-          >Parcerias com</Text>
+            color="#6F6F6F"
+          >Parcerias com</Title>
         </Center>
         <Stack
           direction="row"
@@ -115,7 +114,7 @@ export function BePartner() {
         </Stack>
       </Stack>
       {!isMobile &&
-        <HStack w="110%">
+        <HStack paddingBottom="30px" w="110%">
           <Carousel
             plugins={[
               "infinite",
@@ -134,77 +133,34 @@ export function BePartner() {
               position="Diretora-Executiva do Instituto Alziras"
               src="https://basedosdados-static.s3.us-east-2.amazonaws.com/logos/alziras.png"
             >
-              Com o apoio da Base dos Dados realizamos o{" "}
-              <SectionText 
-                fontFamily="Ubuntu" 
-                fontSize="22px" 
-                fontWeight="500" 
-                textAlign="center"
-                lineHeight="30px"
-              >
-                levantamento das desigualdades de gênero e raça nas eleições 2016-2020 e das distorções de financiamento de campanhas de prefeitas e vereadoras
-              </SectionText>
-              nas últimas eleições, o que resultará no lançamento de uma
-              publicação inédita.
+              Com o apoio da Base dos Dados realizamos o levantamento das 
+              desigualdades de gênero e raça nas eleições 2016-2020 e das 
+              distorções de financiamento de campanhas de prefeitas e vereadoras 
+              nas últimas eleições, o que resultará no lançamento de uma publicação inédita.
             </Testimonial>
             <Testimonial
               name="Amanda de Albuquerque e Mariana Carvalho"
               position="Co-fundadoras dA Ponte"
               src="https://basedosdados-static.s3.us-east-2.amazonaws.com/logos/aponte.png"
             >
-              Criamos um importante produto nosso, a{" "}
-              <SectionText 
-                fontFamily="Ubuntu" 
-                fontSize="22px" 
-                fontWeight="500" 
-                textAlign="center"
-                lineHeight="30px"
-              >
-                Fotografia do Município,{" "}
-              </SectionText>
+              Criamos um importante produto nosso, a Fotografia do Município,
               em parceria com a BD. A Fotografia
               agrega diversos indicadores socioeconômicos dos municípios
               brasileiros. A Ponte fez o planejamento e desenho do relatório,
               enquanto a BD cuidou da parte técnica de visualização e gestão de
-              dados.{" "}
-              <SectionText 
-                fontFamily="Ubuntu" 
-                fontSize="22px" 
-                fontWeight="500" 
-                textAlign="center"
-                lineHeight="30px"
-              >
-                A parceria foi o que nos permitiu avançar rápido no
-                desenvolvimento da ferramenta, e garantiu a qualidade de dados
-                alta como queríamos desde o início.
-              </SectionText>
+              dados. A parceria foi o que nos permitiu avançar rápido no
+              desenvolvimento da ferramenta, e garantiu a qualidade de dados
+              alta como queríamos desde o início.
             </Testimonial>
             <Testimonial
               name="Fernando Barbalho"
               position="Cientista de Dados do Tesouro Nacional"
               src="https://basedosdados-static.s3.us-east-2.amazonaws.com/logos/tesouro_nacional.png"
             >
-              <SectionText 
-                fontFamily="Ubuntu" 
-                fontSize="22px" 
-                fontWeight="500" 
-                textAlign="center"
-                lineHeight="30px"
-              >
-                A siconfiBD é um pacote que traz de forma rápida e programática os
-                dados da Secretaria do Tesouro Nacional.
-              </SectionText>
+              A siconfiBD é um pacote que traz de forma rápida e programática os
+              dados da Secretaria do Tesouro Nacional.
               O código é construído de forma <i>open-source</i> pela equipe do Tesouro
-              e 
-              <SectionText 
-                fontFamily="Ubuntu" 
-                fontSize="22px" 
-                fontWeight="500" 
-                textAlign="center"
-                lineHeight="30px"
-              >
-                utiliza as tabelas tratadas BD+ para compor funções em R.
-              </SectionText>
+              e utiliza as tabelas tratadas BD+ para compor funções em R.
               Qual a despesa de pessoal dos 100 municípios brasileiros mais
               populosos? Essas e mais de outras 18 perguntas podem ser consultadas
               diretamente pelo pacote, sem necessidade de SQL.
