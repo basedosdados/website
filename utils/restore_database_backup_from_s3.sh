5fed7e4116ec9bab4ea613755dc46bfa11be58e6
Guilherme Peres <contact@gperes.dev>
2022-05-25 08:58:36 -0300

    minorFix: typo

--- utils/restore_database_backup_from_s3.sh
+++ utils/restore_database_backup_from_s3.sh
@@ -2,15 +2,13 @@
 
 cd $(git rev-parse --show-toplevel)
 
-if [[ $BD_ENVIRON = 'STAGING' ]]; then
-    echo Downloading dump from S3
+if [[ $BD_ENVIRON != 'PROD' ]]; then
     AWS_DEFAULT_REGION=us-east-2 AWS_PROFILE=basedosdados aws s3 ls s3://basedosdados/backup/postgres.dump
     AWS_DEFAULT_REGION=us-east-2 AWS_PROFILE=basedosdados aws s3 cp s3://basedosdados/backup/postgres.dump /tmp/db-ckan.dump
 
     docker-compose stop
     docker-compose up -d db
 
-    echo Recreating CKAN DB
     DB="docker exec -i -e PGPASSWORD=ckan db"
     $DB /wait-until-up localhost:5432
     $DB bash -c 'dropdb -U ckan ckan --if-exists && createdb -U ckan ckan'
