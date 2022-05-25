cb7f23eb1af386624de53311b59774b1cc2dda52
Guilherme Peres <contact@gperes.dev>
2022-05-25 08:38:32 -0300

    added script to restore database from s3

---
+++ utils/restore_database_backup_from_s3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd $(git rev-parse --show-toplevel)
+
+if [[ $STAGING ]]; then
+    echo Downloading dump from S3
+    AWS_DEFAULT_REGION=us-east-2 AWS_PROFILE=basedosdados aws s3 ls s3://basedosdados/backup/postgres.dump
+    AWS_DEFAULT_REGION=us-east-2 AWS_PROFILE=basedosdados aws s3 cp s3://basedosdados/backup/postgres.dump /tmp/db-ckan.dump
+
+    docker-compose stop
+    docker-compose up -d db
+
+    echo Recreating CKAN DB
+    DB="docker exec -i -e PGPASSWORD=ckan db"
+    $DB /wait-until-up localhost:5432
+    $DB bash -c 'dropdb -U ckan ckan --if-exists && createdb -U ckan ckan'
+    $DB pg_restore -U ckan -d ckan --format=custom --exit-on-error < /tmp/db-ckan.dump
+
+    docker-compose up -d
+    docker exec -it ckan ckan search-index rebuild --verbose
+fi
