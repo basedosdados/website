2f20ed5f3ec0677c8a44bb509df35f665a867c17
Guilherme Peres <contact@gperes.dev>
2022-05-25 14:39:49 -0300

    minorFix: Conditional to build_config

--- deploy.sh
+++ deploy.sh
@@ -47,7 +47,7 @@ build_config() {
     cp configs/basedosdados_crontab build/basedosdados_crontab
     cp configs/bashrc build/
 
-    if [[ $BD_ENVIRON == "STAGING" ]]; then
+    if [[ $HOST == "staging" ]]; then
         cp util/restore_database_backup_from_s3.sh build/restore_database_backup_from_s3.sh
         cat configs/basedosdados_crontab_staging >> build/basedosdados_crontab
     fi
