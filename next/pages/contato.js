17dfbb15764002ea8d60465fefbad71bdea764ef
AldemirLucas <aldemirlucas23@gmail.com>
2022-06-28 20:40:21 -0300

    wip ajust transparencia

--- next/pages/contato.js
+++ next/pages/contato.js
@@ -52,48 +52,49 @@ export default function Contato({ pages }) {
             >
               Entre em contato
             </Display>
-            <SectionText paddingBottom="24px">
+            <SectionText fontSize="16px" paddingBottom="24px">
               Envie sua mensagem no formulário ao lado para falar com nossa equipe.
             </SectionText>
             <Subtitle paddingBottom="8px">
               Apoio institucional
             </Subtitle>
-            <SectionText paddingBottom="24px">
+            <SectionText fontSize="16px" paddingBottom="24px">
               A Base dos Dados é mantida por meio doações de pessoas que acreditam na transparência e acesso a dados de qualidade. Colabore para o crescimento e fortalecimento da iniciativa.
             </SectionText>
             <Subtitle paddingBottom="8px">
               Serviços
             </Subtitle>
-            <SectionText paddingBottom="24px">
+            <SectionText fontSize="16px" paddingBottom="24px">
               Fale diretamente com nossa equipe comercial para marcarmos uma conversa e avaliarmos como{" "}
               <Link
                 href="/servicos"
                 textDecoration="none"
                 color="#42B0FF"
+                fontSize="16px"
               >
-                nossos serviços{" "}
-              </Link>
-              podem ajudar.
+                nossos serviços
+              </Link> podem ajudar.
             </SectionText>
             <Subtitle paddingBottom="8px">
               Projetos e Parcerias
             </Subtitle>
-            <SectionText paddingBottom="24px">
+            <SectionText fontSize="16px" paddingBottom="24px">
               Desenvolvemos projetos e aplicações com outras organizações afins para promover o acesso a dados públicos de qualidade. Estamos sempre abertos a novas ideias.
             </SectionText>
             <Subtitle paddingBottom="8px">
               Dados
             </Subtitle>
-            <SectionText paddingBottom="24px">
+            <SectionText fontSize="16px" paddingBottom="24px">
               Auxiliamos pessoas e organizações a subirem dados no nosso <i>datalake</i>. Escreva sua proposta para nossa equipe de Dados.
             </SectionText>
-            <SectionText paddingBottom="24px" fontWeigth="500">
+            <SectionText fontSize="16px" paddingBottom="24px" fontWeigth="500">
               Dúvidas? Fale com a nossa comunidade no{" "}
               <Link
                 href="https://discord.gg/huKWpsVYx4"
                 textDecoration="none"
                 color="#42B0FF"
                 target="_blank"
+                fontSize="16px"
               >
                 Discord<a style={{color:"#252A32", fontWeight:"500"}}>.</a>
               </Link>
--- next/pages/transparencia.js
+++ next/pages/transparencia.js
@@ -44,7 +44,7 @@ export default function Transparencia({ pages }) {
     )
   }
 
-  const GraphicsBox = ({ text, url }) => {
+  const GraphicsBox = ({ text, url, ...props }) => {
     return (
       <Box
         display="flex"
@@ -53,20 +53,24 @@ export default function Transparencia({ pages }) {
         maxWidth={{ base: "100%", lg: "45%" }}
         width={{base: "350px", lg: "650px"}}
         height={{base: "320px", lg: "400px"}}
+        {...props}
       >
-        <Text
-          width="100%"
-          fontFamily="Ubuntu"
-          fontSize={{base: "16px", lg: "22px"}}
-          fontWeigth="400"
-          letterSpacing={{base: "0.2px", lg: "0.1px"}}
-          minHeight="30px"
-          marginBottom={{base: "0px", lg: "24px"}}
-          color="#252A32"
-        >
-          {text}
-        </Text>
-        
+        {text &&
+          <Text
+            width="100%"
+            fontFamily="Ubuntu"   
+            textAlign={isMobileMod ? "start" : "center"}
+            fontSize={{base: "16px", lg: "22px"}}
+            fontWeigth="400"
+            letterSpacing={{base: "0.2px", lg: "0.1px"}}
+            minHeight="30px"
+            marginBottom={{base: "0px", lg: "32px"}}
+            color="#252A32"
+          >
+            {text}
+          </Text>
+        }
+
         <iframe
           src={url}
           frameborder="0"
@@ -94,10 +98,10 @@ export default function Transparencia({ pages }) {
           >
             Nossas contas são transparentes e abertas – como todo o resto.
           </Display>
-          <SectionText paddingBottom="20px">
+          <SectionText fontSize="16px" paddingBottom="20px">
             Somos uma organização não-governamental sem fins lucrativos e <i>open source</i>. A transparência fundamenta todas as nossas ações, desde o trabalho com dados públicos até a prestação de contas. Utilizamos os recursos para facilitar o acesso de milhares de pessoas a dados de qualidade. 
           </SectionText>
-          <SectionText paddingBottom="20px">
+          <SectionText fontSize="16px" paddingBottom="20px">
             Nesse espaço, você encontra informações referentes às nossas atividades, receitas e despesas. Acompanhe de perto como garantimos a sustentabilidade da organização.
           </SectionText>
         </Stack>
@@ -152,68 +156,98 @@ export default function Transparencia({ pages }) {
           >
             Indicador de sobrevida
           </BigTitle>
-          <SectionText paddingBottom="20px">
+          <SectionText fontSize="16px" paddingBottom="20px">
             Esse é nosso indicador de sobrevida. Ele mostra o tempo que nossas atividades poderiam se manter caso a arrecadação de recursos financeiros fosse interrompida, considerando as despesas regulares com infraestrutura e equipe, por exemplo. 
           </SectionText>
-          <SectionText paddingBottom="20px">
-            O indicador evidencia a importância de sua contribuição para a continuidade dos nossos esforços em busca da universalização do acesso a dados públicos.
-          </SectionText>
-          <SectionText paddingBottom="20px">
-            Abaixo, você confere as fontes e destinações dos recursos da organização. Como não poderia deixar de ser, todos os microdados relativos à contabilidade da BD estão disponíveis no nosso <i>datalake</i> público. Veja
-            <Link
-              color="#42B0FF"
-              href={`https://basedosdados.org/dataset/br-bd-indicadores?bdm_table=contabilidade#titled=false&bordered=false`}
-            > aqui
-            </Link>.
+          <SectionText fontSize="16px" paddingBottom="20px">
+            O indicador evidencia a importância de sua contribuição para a continuidade dos nossos esforços em busca da universalização do acesso a dados públicos. Abaixo, você confere detalhes sobre as fontes e destinações dos recursos da organização.
           </SectionText>
         </Stack>
+      </SectionBox>
+
+      <SectionBox
+        flexDirection="column"
+        paddingTop={{ base: "60px", lg: "144px" }}
+        alignItems={isMobileMod ? "start" : "center"}
+      >
+        <BigTitle
+          textAlign={isMobileMod ? "start" : "center"}
+          paddingBottom="8px"
+        >
+          Dados da contabilidade
+        </BigTitle>
+        <Text
+          fontFamily="ubuntu"
+          fontSize={isMobileMod ? "16px" : "18px"}
+          textAlign={isMobileMod ? "start" : "center"}
+          paddingBottom="24px"
+          fontWeight="300"
+          letterSpacing="0.1px"
+          lineHeight={isMobileMod ? "24px" : "26px"}
+          color="#252A32"
+        >
+          Como não poderia deixar de ser, todos os microdados{isMobileMod ? " " : <br/>}
+          relativos à contabilidade da BD estão disponíveis no <i>datalake</i> público.
+        </Text>
+        <RoundedButton
+          width="fit-content"
+          onClick={() => window.open(
+            "https://basedosdados.org/dataset/br-bd-indicadores?bdm_table=contabilidade", "_blank"
+          )}
+        >
+          Acesse
+        </RoundedButton>
       </SectionBox>
 
       <SectionBox
         order={isMobileMod ? 0 : 1}
-        paddingTop={{ base: "40px", lg: "160px" }}
-        gridGap={{ base: "40px", lg: "0px" }}
+        paddingTop={{ base: "40px", lg: "64px" }}
+        flexDirection="column"
       >
-        <GraphicsBox 
-          text= "Receitas acumuladas"
-          url= "https://perguntas.basedosdados.org/public/question/c41beae5-94d3-41e2-9161-a12492b0cae0#titled=false&bordered=false"
-        />
+        <Stack
+          flexDirection={{ base: "column", lg: "row" }}
+          justifyContent="center"
+          gridGap={{ base: "40px", lg: "80px" }}
+        >
+          <GraphicsBox
+            text="Receitas acumuladas"
+            url="https://perguntas.basedosdados.org/public/question/c41beae5-94d3-41e2-9161-a12492b0cae0#titled=false&bordered=false"
+          />
 
-        <GraphicsBox 
-          text= "Despesas acumuladas"
-          url= "https://perguntas.basedosdados.org/public/question/312842db-4ea9-455b-be7a-98d96e742ea7#titled=false&bordered=false"
-        />
+          <GraphicsBox 
+            text="Despesas acumuladas"
+            url="https://perguntas.basedosdados.org/public/question/312842db-4ea9-455b-be7a-98d96e742ea7#titled=false&bordered=false"
+          />
+        </Stack>
+
+        <SectionText marginRight="24px" paddingTop="40px" textAlign={isMobileMod ? "start" : "end"} fontSize={isMobileMod ? "14px" : "16px"}>
+          Acesse o painel completo <Link fontSize={isMobileMod ? "14px" : "16px"} target="_blank" color="#42B0FF" href="https://perguntas.basedosdados.org/public/dashboard/ab21da85-bff2-435b-a819-953d785167b4"
+          > aqui</Link>.
+        </SectionText>
       </SectionBox>
 
       <SectionBox
-        paddingTop={{ base: "80px", lg: "160px" }}
+        paddingTop={{ base: "80px", lg: "104px" }}
+        flexDirection="column"
       >
-        <Stack
-          paddingTop={{ base: "0px", lg: "24px" }}
-          maxWidth={{ base: "100%", lg: "45%" }}
+        <BigTitle
+          paddingBottom={{base: "16px", lg: "32px"}}
         >
-          <BigTitle
-            paddingBottom={{base: "16px", lg: "24px"}}
-          >
-            Estatuto Social
-          </BigTitle>
-          <SectionText paddingBottom="20px">
-            Em nosso estatuto, estão presentes as diretrizes que regulamentam o funcionamento e o processo de tomada de decisões da organização, além dos direitos e deveres dos membros e dos conselhos administrativos e fiscais.
-          </SectionText>
-          <SectionText paddingBottom="20px">
-            O documento estabelece também os meios pelos quais sustentamos a iniciativa e os procedimentos relacionados à prestação de contas. É a partir do estatuto que norteamos o aprimoramento das nossas ferramentas e o desenvolvimento de novos projetos para a organização.
-          </SectionText>
-        </Stack>
+          Estatuto e relatórios 
+        </BigTitle>
 
         <Stack
-          paddingTop={{ base: "0px", lg: "96px" }}
-          maxWidth={{ base: "100%", lg: "45%" }}
+          width="100%"
+          flexDirection={isMobileMod ? "column" : "row"}
+          justifyContent="space-between"
+          spacing={0}
         >
-          <SectionText paddingBottom="20px">
-            Os membros são pessoas que firmam compromisso com a nossa missão através de pedido por escrito feito ao Conselho de Administração. Uma vez associados, eles possuem direito formal de voto nas questões que direcionam a organização rumo à concretização dos objetivos sociais.
+          <SectionText maxWidth={isMobileMod ? "100%" : "45%"}  fontSize="16px" paddingBottom="20px">
+            Em nosso estatuto, estão presentes as diretrizes que regulamentam o funcionamento e o processo de tomada de decisões da organização. O documento inclui também os direitos e deveres dos membros e as competências dos conselhos administrativos e fiscais.
           </SectionText>
-          <SectionText paddingBottom={{base: "20px", lg: "0px"}}>
-            Os conselhos administrativos e fiscais são renovados através de eleições orientadas pelas regras do estatuto. Esses órgãos são responsáveis pela aprovação dos relatórios anuais de atividades, que posteriormente serão submetidos para apreciação da Assembleia Geral.
+        
+          <SectionText maxWidth={isMobileMod ? "100%" : "45%"} fontSize="16px" paddingBottom="20px">
+            Nossos relatórios apresentam todas as atividades empenhadas pela equipe. São diversos projetos que colaboram com a promoção da cultura de transparência, o desenvolvimento socioeconômico e a construção de políticas públicas baseadas em dados e evidências.
           </SectionText>
         </Stack>
       </SectionBox>
@@ -225,37 +259,14 @@ export default function Transparencia({ pages }) {
         margin="auto"
       >
         <Link
+          fontSize="16px"
           target="_blank"
           color="#42B0FF"
-          href={`https://basedosdados-static.s3.us-east-2.amazonaws.com/pdf/BD_Estatuto_Social.pdf`}
+          href="https://basedosdados-static.s3.us-east-2.amazonaws.com/pdf/BD_Estatuto_Social.pdf"
         >
           BD Estatuto Social
         </Link>
       </Stack>
-      
-      <SectionBox
-        paddingTop={{ base: "80px", lg: "160px" }}
-      >
-        <Stack maxWidth={{ base: "100%", lg: "45%" }}>
-          <BigTitle
-            paddingBottom={{base: "16px", lg: "24px"}}
-          >
-            Relatórios anuais de atividades
-          </BigTitle>
-          <SectionText paddingBottom="20px">
-            Nossos relatórios apresentam todas as atividades empenhadas pela equipe. São diversos projetos que colaboram com a promoção da cultura de transparência, o desenvolvimento socioeconômico e a construção de políticas públicas baseadas em dados e evidências.
-          </SectionText> 
-        </Stack>
-
-        <Stack 
-          maxWidth={{ base: "100%", lg: "45%" }}
-          paddingTop={{ base: "0", lg: "72px" }}
-        >
-          <SectionText paddingBottom="20px">
-            Os documentos reúnem também as demonstrações de desempenho financeiro da organização. Explore nossos relatórios e conheça melhor o trabalho desenvolvido nas diferentes frentes de atuação.
-          </SectionText> 
-        </Stack>
-      </SectionBox>
      
       <Stack
         paddingTop="16px"
@@ -265,9 +276,10 @@ export default function Transparencia({ pages }) {
         margin="auto"
       >
         <Link
+          fontSize="16px"
           target="_blank"
           color="#42B0FF"
-          href={`https://basedosdados-static.s3.us-east-2.amazonaws.com/pdf/BD_Relatorio_Anual_2021.pdf`}
+          href="https://basedosdados-static.s3.us-east-2.amazonaws.com/pdf/BD_Relatorio_Anual_2021.pdf"
         >
           BD Relatório Anual 2021
         </Link>
@@ -299,21 +311,22 @@ export default function Transparencia({ pages }) {
             >
               Você também acredita no <p>acesso a dados de qualidade?</p>
             </BigTitle>
-            <SectionText 
+            <SectionText
+              fontSize="16px"
               paddingBottom="20px"
               color="#FFF"
             >
               Tudo o que fazemos só é possível por conta das pessoas que apoiam o nosso trabalho. Ajude a BD a continuar facilitando o acesso a dados públicos. Com qualquer valor, você contribui para a manutenção dos nossos projetos e a sobrevivência da organização.
             </SectionText>
-            <Link _hover={{ opacity:"none" }} target="_blank" href="https://apoia.se/basedosdados">
-              <RoundedButton
-                backgroundColor="#FF8484"
-                height="35px"
-                fontSize="14px"
-              >
-                Apoie agora
-              </RoundedButton>
-            </Link>
+            <RoundedButton
+              backgroundColor="#FF8484"
+              width="fit-content"
+              height="35px"
+              fontSize="14px"
+              onClick={() => window.open("https://apoia.se/basedosdados", "_blank")}
+            >
+              Apoie agora
+            </RoundedButton>
           </Stack>
 
           <Stack 
