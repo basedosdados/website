eebd2f0f31631e0e51d655100124f6d2d58a71c7
AldemirLucas <aldemirlucas23@gmail.com>
2022-07-05 17:33:08 -0300

    add teamBox

--- next/pages/quem-somos.js
+++ next/pages/quem-somos.js
@@ -1,8 +1,8 @@
 import {
   Box,
   Center,
-  VStack,
   Stack,
+  VStack,
   Text,
 } from "@chakra-ui/react";
 import React, { useState, useEffect} from "react";
@@ -13,12 +13,16 @@ import Display from "../components/atoms/Display";
 import SectionText from "../components/atoms/SectionText";
 import Link from "../components/atoms/Link";
 import Carousel from "../components/atoms/Carousel";
+import WebIcon  from "../public/img/icons/webIcon";
+import TwitterIcon  from "../public/img/icons/twitterIcon";
+import LinkedinIcon  from "../public/img/icons/linkedinIcon";
+import GitIcon  from "../public/img/icons/gitIcon";
 
 export async function getStaticProps(context) {
   return await withPages();
 }
 
-const HistoryBox = () => {
+const HistoryBox = ({ title, date, text }) => {
   const isMobile = useCheckMobile();
   const [isMobileMod, setIsMobileMod] = useState(false)
   
@@ -35,8 +39,8 @@ const HistoryBox = () => {
       <Box
         backgroundColor="#CECECE"
         borderRadius="20px"
-        width={isMobileMod ? "70vw" : "500px"}
-        height="300px"
+        width={isMobileMod ? "90vw" : "500px"}
+        height={isMobileMod ? "200px" : "300px"}
         boxShadow="0 2px 20px 0 #00000026"
       >
 
@@ -50,10 +54,101 @@ const HistoryBox = () => {
           fontSize="20px"
           letterSpacing="0.2px"
           marginBottom="8px"
-        >Adicionar Titulo</Text>
-        <SectionText fontSize="16px" color="#6F6F6F" marginBottom="16px">Adicionar Data</SectionText>
+        >{title}</Text>
+        <SectionText fontSize="16px" color="#6F6F6F" marginBottom="16px">{date}</SectionText>
         
-        <SectionText fontSize="16px" >Adicionar Texto</SectionText>
+        <SectionText fontSize="16px" >{text}</SectionText>
+      </Box>
+    </Box>
+  )
+}
+
+const team = {
+  "nome-do-time" : [
+    {
+      name:"Adicionar Nome",
+      role:"Adicionar Funcoes",
+      description:"Adicionar Descricao",
+      website:"Adicionar Site",
+      twitter:"Adicionar conta do twitter",
+      linkedin:"Adicionar conta do linkedin",
+      github:"Adicionar usuario Github"
+    }
+  ]
+}
+
+const TeamBox = ({ index, bio }) => {
+  const isMobile = useCheckMobile();
+  const [isMobileMod, setIsMobileMod] = useState(false)
+  
+  useEffect(() => {
+    setIsMobileMod(isMobile)
+  }, [isMobile])
+
+  const hasLeftSpacing = (index % 2 == 0) ? false : true
+
+  const keyIcons = (ref) => {
+    let href = ""
+
+    if(ref.website) { href = `https://${ref.website}` }
+    if(ref.twitter) {
+      const twitter = ref.twitter.replace(/(https:)\/\/(twitter.com)\//gim, "")
+      href = `https://twitter.com/${twitter}`
+    }
+    if(ref.linkedin) {
+      const linkedin = ref.linkedin.replace(/(https:)\/\/(linkedin.com)\/(in)\//gim, "")
+      href = `https://linkedin.com/in/${linkedin}`
+    }
+    if(ref.github) {
+      const github = ref.github.replace(/(https:)\/\/(github.com)\//gim, "")
+      href = `https://github.com/${github}` 
+    }
+
+    return {
+      cursor: "pointer",
+      widthIcon:"22px",
+      heightIcon:"22px",
+      fill: "#42B0FF",
+      onClick: () => {window.open(href)}
+    }
+  }
+
+  const iconLinks = () => {
+    return (
+      <Box display="flex" flexDirection="row" gridGap="5px">
+        {bio.website ? <WebIcon {...keyIcons({website: bio.website})}/> : null}
+        {bio.twitter ? <TwitterIcon {...keyIcons({twiiter: bio.twitter})}/> : null}
+        {bio.linkedin ? <LinkedinIcon {...keyIcons({linkedin: bio.linkedin})}/> : null}
+        {bio.github ? <GitIcon {...keyIcons({github: bio.github})}/> : null}
+      </Box>
+    )
+  }
+
+  return (
+    <Box
+      display="flex"
+      flexDirection={isMobileMod ? "column" : "row"}
+      maxWidth="900px"
+      marginLeft={isMobileMod ? "" : hasLeftSpacing && "200px !important"}
+    >
+      <Box
+        backgroundColor="#F0F0F0"
+        minWidth="160px"
+        maxWidth="160px"
+        minHeight="160px"
+        maxHeight="160px"
+        borderRadius="16px"
+        marginRight="32px"
+        marginBottom={isMobileMod && "20px"}
+      />
+      <Box display="flex" flexDirection="column">
+        <Box marginBottom="8px" display="flex" flexDirection="row">
+          <SectionText fontSize="18px" fontFamily="ubuntu" color="#6F6F6F" marginRight="16px">{bio.name}</SectionText>
+          {!isMobileMod && iconLinks()}
+        </Box>
+        <SectionText marginBottom="16px" fontSize="18px" fontFamily="ubuntu">{bio.role}</SectionText>
+        <SectionText marginBottom="16px" fontSize="16px">{bio.description}</SectionText>
+        {isMobileMod && iconLinks()}
       </Box>
     </Box>
   )
@@ -129,6 +224,7 @@ export default function QuemSomos({ pages }) {
         alignItems="center"
         position="relative"
         left={isMobileMod ? "-24px" : "-32px"}
+        marginBottom="160px"
         bgGradient="linear(#34A15A 62%, #FFF 62%)"
       >
         <Center flexDirection="column">
@@ -152,22 +248,44 @@ export default function QuemSomos({ pages }) {
         >
           <Carousel
             settings={{
-              spaceBetween: isMobileMod ? 10 : 400,
+              spaceBetween: isMobileMod ? 100 : 400,
               slidesPerView: isMobileMod ? 1 : 3,
               centeredSlides: true,
               mousewheel: true
             }}
           >
-            <HistoryBox/>
-            <HistoryBox/>
-            <HistoryBox/>
-            <HistoryBox/>
-            <HistoryBox/>
-            <HistoryBox/>
-            <HistoryBox/>
-            <HistoryBox/>
+            <HistoryBox
+              title="Adicionar Titulo"
+              date="Adicionar Data"
+              text="Adicionar Texto"
+            />
+            <HistoryBox
+              title="Adicionar Titulo"
+              date="Adicionar Data"
+              text="Adicionar Texto"
+            />
+            <HistoryBox
+              title="Adicionar Titulo"
+              date="Adicionar Data"
+              text="Adicionar Texto"
+            />
           </Carousel>
         </Center>
+      </Stack>
+
+      <Stack 
+        width="100%"
+        maxWidth="1264px"
+        spacing="100px"
+        paddingBottom="80px"
+        margin="auto"
+      >
+        {team["nome-do-time"].map((elm, index) => (
+          <TeamBox
+            index={index}
+            bio={elm}
+          />
+        ))}
       </Stack>
 
     </MainPageTemplate>
