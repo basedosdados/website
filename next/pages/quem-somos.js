8bf237f341aa660574924b8bb659d923d94ec58d
AldemirLucas <aldemirlucas23@gmail.com>
2022-07-14 18:17:01 -0300

    finish shortcut bar

--- next/pages/quem-somos.js
+++ next/pages/quem-somos.js
@@ -2,6 +2,7 @@ import {
   Box,
   Center,
   Stack,
+  VStack,
   Image,
   Text,
 } from "@chakra-ui/react";
@@ -58,7 +59,6 @@ const HistoryBox = ({ title, date, text }) => {
           marginBottom="8px"
         >{title}</Text>
         <SectionText fontSize="16px" color="#6F6F6F" marginBottom="16px">{date}</SectionText>
-        
         <SectionText fontSize="16px" >{text}</SectionText>
       </Box>
     </Box>
@@ -209,7 +209,7 @@ export default function QuemSomos({ pages }) {
       heightIcon:"20px",
       fill: "#42B0FF",
       padding: "8px 12px 8px",
-      boxShadow: "0 1px 0 0 #0000001a",
+      boxShadow: "1px 1px 0 0 #0000001a",
       _hover: {opacity: "0.8"},
       onClick: () => {window.open(url)}
     }
@@ -217,144 +217,102 @@ export default function QuemSomos({ pages }) {
 
   return (
     <MainPageTemplate pages={pages} paddingX="24px">
-      {/* <Stack
-        paddingTop={{ base: "80px", lg: "0px" }}
-        paddingBottom={{ base: "10px", lg: "50px" }}
-        width="100%"
-        maxWidth="1264px"
-        justifyContent="space-between"
-        direction={{ base: "column", lg: "row" }}
-        margin="auto"
-      >
-        <VStack maxWidth={{ base: "100%", lg: "45%" }}>
-          <Box contentAlign="flex-start">
-            <Display 
-              color="#2B8C4D"
-              paddingBottom="24px"
-            >
-              Quem somos
-            </Display>
-            <SectionText fontSize="14px" fontWeight="300" paddingBottom="20px">
-              A Base dos Dados é uma organização não-governamental, sem fins lucrativos, <i>open source</i> e colaborativa. Nossa missão é universalizar o acesso a dados públicos de qualidade para que <b>a distância entre qualquer pessoa e uma análise seja apenas uma boa pergunta</b>.
-            </SectionText>
-            <SectionText fontSize="14px" fontWeight="300" paddingBottom="20px">
-              Atuamos nesse objetivo catalogando e organizando bases de dados públicas que podem ser encontradas através do nosso mecanismo de busca, com informações estruturadas dos conjuntos de dados e a possibilidade de download direto.
-            </SectionText>
-            <SectionText fontSize="14px" fontWeight="300" paddingBottom="20px">
-              Além disso, nossa iniciativa conta com um <i>datalake</i> público, em que disponibilizamos diversas <b>bases de dados já limpas</b>, <b>integradas e compatibilizadas</b>. Através dele, você pode acessar e cruzar tabelas de diferentes organizações de maneira simples e rápida usando <b>nossos pacotes</b> em <b>Python</b>, <b>R</b> ou direto pelo <b>BigQuery</b>. Nós estruturamos dados públicos para você poder focar no que realmente importa, sua análise (artigo científico, matéria de jornal, painel de visualização, integrações etc).
-            </SectionText>
-          </Box>
-        </VStack>
-        <VStack maxWidth={{ base: "100%", lg: "45%" }}>
-          <Box 
-            contentAlign="flex-start" 
-            paddingTop={{ base: "0px", lg: "80px" }}
-          >
-            <SectionText fontSize="14px" fontWeight="300" paddingBottom="20px">
-              A ideia de catalogar bases de dados importantes começou em <b>2019</b>, com nosso co-fundador <b>Ricardo Dahis</b>, que enxergou a necessidade de uma plataforma com capacidade de busca e filtragem de diferentes conjuntos de dados e iniciou esse trabalho.
-            </SectionText>
-            <SectionText fontSize="14px" fontWeight="300" paddingBottom="20px">
-              Mediante uma proposta do <b>João Carabetta</b>, também co-fundador da Base dos Dados, para estruturar um <i>datalake</i> pelo BigQuery e usar o mecanismo de busca para catalogação dos metadados, nossa iniciativa começou a tomar forma. 
-            </SectionText>
-            <SectionText fontSize="14px" fontWeight="300" paddingBottom="20px">
-              Depois de um trabalho em conjunto estruturando as primeiras bases de dados com a <b>Fernanda Scovino</b>, <b>Fred Israel</b> e <b>Diego Oliveira</b>, todos co-fundadores da BD, lançamos nosso <i>datalake</i> público em <b>outubro de 2020</b>. Desde então, nossa organização vem crescendo em qualidade e quantidade de dados, número de colaboradores, parceiros e usuários. Faça parte dessa história também!
-            </SectionText>
-            <Link
-              href="https://drive.google.com/file/d/1JNcr4psQr42EV9mTafn512TD8p87ZP3a/view?usp=sharing"
-              textDecoration="none"
-              color="#42B0FF"
-              target="_blank"
-            >
-              Veja nosso estatuto.
-            </Link>
-          </Box>
-        </VStack>
-      </Stack> */}
       <Stack
-        spacing={0}
-        position="fixed"
-        boxShadow="1px -1px 0 0 #0000001a"
-        top="40%"
-        left="0"
+        position="relative"
+        left={isMobileMod ? "0" :"-24px"}
       >
-        <TwitterIcon {...keyIcon("https://twitter.com/basedosdados")}/>
-        <DiscordIcon {...keyIcon("https://discord.gg/huKWpsVYx4")}/>
-        <GitIcon {...keyIcon("https://github.com/basedosdados")}/>
-        <LinkedinIcon {...keyIcon("https://www.linkedin.com/company/base-dos-dados/mycompany/")}/>
-      </Stack>
-
-      <Stack
-        width="100%"
-        maxWidth="1264px"
-        margin="auto"
-        paddingTop={{ base: "80px", lg: "0px" }}
-        alignItems="center"
-        paddingBottom="200px"
-      >
-        <Display lineHeight="52px" textAlign="center" marginBottom="56px">
-          Facilitamos o acesso a dados<br/> para que a distância entre você e sua análise <br/>seja <a style={{color:"#2B8C4D"}}>apenas uma boa pergunta</a>.
-        </Display>
-        <Text
-          width={isMobileMod ? "100%" : "80%"}
-          fontFamily="ubuntu"
-          fontWeight="300"
-          fontSize="20px"
-          lineHeight="32px"
-          letterSpacing="0.2px"
-          color="#6F6F6F"
-          textAlign="center"
-        >
-          Adicionar subtitle: Milhares de pessoas encontram, baixam, cruzam e analisam dados de um jeito 
-          muito mais fácil com a BD. Vivamus a urna vestibulum, luctus erat vitae, rhoncus ante. 
-        </Text>
-      </Stack>
-
-      <Stack
-        width="100%"
-        maxWidth="1264px"
-        margin="auto"
-        paddingBottom="160px"
-        spacing={0}
-      >
-        <Center marginBottom="80px" flexDirection="column">
-          <TrophySvg width="100px" height="100px" marginBottom="8px"/>
-          <BigTitle>Reconhecimentos</BigTitle>
-        </Center>
-
         <Stack
-          flexDirection={isMobileMod ? "column" : "row"}
-          justifyContent="space-between"
-          spacing={isMobileMod ? "120px" : "0"}
+          display={isMobileMod ? "none" :"flex"}
+          spacing={0}
+          position="sticky"
+          width="fit-content"
+          height="0"
+          top="40%"
+          left="-32px"
+          backgroundColor="#FFF"
         >
-          <Box textAlign="center" maxWidth={isMobileMod ? "100%" : "45%"}>
-            <Image
-              src="https://basedosdados-static.s3.us-east-2.amazonaws.com/logos/2022/google_cloud.svg"
-              width="140px"
-              height="140px"
-              margin="0 auto 48px"
-            />
-            <SectionTitle marginBottom="16px" fontSize="22px">Google Cloud Customer Award</SectionTitle>
-            <SectionText fontSize="16px">O prêmio reconheceu as implementações mais inovadoras e transformadoras do Google Cloud ao redor do mundo. 
-                Fomos a única organização brasileira a receber a premiação na categoria de Impacto Social, que também selecionou outras iniciativas que usam tecnologia para promover mais abertura e transparência. 
-                O anúncio aconteceu no Google Cloud Next'21. 
-            </SectionText>
-            <SectionText fontWeigth="700" fontSize="16px" color="#42B0FF">Veja mais detalhes.</SectionText>
-          </Box>
-
-          <Box textAlign="center" maxWidth={isMobileMod ? "100%" : "45%"}>
-            <Image
-              src="https://basedosdados-static.s3.us-east-2.amazonaws.com/logos/2022/premio_tesouro_nacional_2021.png"
-              width="140px"
-              height="140px"
-              margin="0 auto 48px"
-            />
-            <SectionTitle marginBottom="16px" fontSize="22px">XXVI Prêmio Tesouro Nacional 2021</SectionTitle>
-            <SectionText fontSize="16px">
-              Conquistamos o 1º lugar na categoria Soluções. O prêmio tem como objetivo expandir as fronteiras do conhecimento em finanças públicas, promovendo a normalização de temas específicos quando tratados consistentemente pela pesquisa científica. Fomos selecionados por conta de nosso trabalho compatibilizando informações de despesas e receitas orçamentárias do Setor Público Brasileiro.
-            </SectionText>
-            <SectionText fontWeigth="700" fontSize="16px" color="#42B0FF">Veja mais detalhes.</SectionText>
-          </Box>
+          <TwitterIcon {...keyIcon("https://twitter.com/basedosdados")} borderTop="1px solid #0000001a"/>
+          <DiscordIcon {...keyIcon("https://discord.gg/huKWpsVYx4")}/>
+          <GitIcon {...keyIcon("https://github.com/basedosdados")}/>
+          <LinkedinIcon {...keyIcon("https://www.linkedin.com/company/base-dos-dados/mycompany/")}/>
         </Stack>
+
+        <VStack paddingLeft={isMobileMod ? "0" : "24px"}>
+          <Stack
+            width="100%"
+            maxWidth="1264px"
+            margin="auto"
+            paddingTop={{ base: "80px", lg: "0px" }}
+            alignItems="center"
+            paddingBottom="200px"
+          >
+            <Display lineHeight="52px" textAlign="center" marginBottom="56px">
+              Facilitamos o acesso a dados<br/> para que a distância entre você e sua análise <br/>seja <a style={{color:"#2B8C4D"}}>apenas uma boa pergunta</a>.
+            </Display>
+            <Text
+              width={isMobileMod ? "100%" : "80%"}
+              fontFamily="ubuntu"
+              fontWeight="300"
+              fontSize="20px"
+              lineHeight="32px"
+              letterSpacing="0.2px"
+              color="#6F6F6F"
+              textAlign="center"
+            >
+              Adicionar subtitle: Milhares de pessoas encontram, baixam, cruzam e analisam dados de um jeito 
+              muito mais fácil com a BD. Vivamus a urna vestibulum, luctus erat vitae, rhoncus ante. 
+            </Text>
+          </Stack>
+
+          <Stack
+            width="100%"
+            maxWidth="1264px"
+            margin="auto"
+            paddingBottom="160px"
+            spacing={0}
+          >
+            <Center marginBottom="80px" flexDirection="column">
+              <TrophySvg width="100px" height="100px" marginBottom="8px"/>
+              <BigTitle>Reconhecimentos</BigTitle>
+            </Center>
+
+            <Stack
+              flexDirection={isMobileMod ? "column" : "row"}
+              justifyContent="space-between"
+              spacing={isMobileMod ? "120px" : "0"}
+            >
+              <Box textAlign="center" maxWidth={isMobileMod ? "100%" : "45%"}>
+                <Image
+                  src="https://basedosdados-static.s3.us-east-2.amazonaws.com/logos/2022/google_cloud.svg"
+                  width="140px"
+                  height="140px"
+                  margin="0 auto 48px"
+                />
+                <SectionTitle marginBottom="16px" fontSize="22px">Google Cloud Customer Award</SectionTitle>
+                <SectionText fontSize="16px">O prêmio reconheceu as implementações mais inovadoras e transformadoras do Google Cloud ao redor do mundo. 
+                    Fomos a única organização brasileira a receber a premiação na categoria de Impacto Social, que também selecionou outras iniciativas que usam tecnologia para promover mais abertura e transparência. 
+                    O anúncio aconteceu no Google Cloud Next'21. 
+                </SectionText>
+                <SectionText fontWeigth="700" fontSize="16px" color="#42B0FF">Veja mais detalhes.</SectionText>
+              </Box>
+
+              <Box textAlign="center" maxWidth={isMobileMod ? "100%" : "45%"}>
+                <Image
+                  src="https://basedosdados-static.s3.us-east-2.amazonaws.com/logos/2022/premio_tesouro_nacional_2021.png"
+                  width="140px"
+                  height="140px"
+                  margin="0 auto 48px"
+                />
+                <SectionTitle marginBottom="16px" fontSize="22px">XXVI Prêmio Tesouro Nacional 2021</SectionTitle>
+                <SectionText fontSize="16px">
+                  Conquistamos o 1º lugar na categoria Soluções. O prêmio tem como objetivo expandir as fronteiras do conhecimento em finanças públicas, promovendo a normalização de temas específicos quando tratados consistentemente pela pesquisa científica. Fomos selecionados por conta de nosso trabalho compatibilizando informações de despesas e receitas orçamentárias do Setor Público Brasileiro.
+                </SectionText>
+                <SectionText fontWeigth="700" fontSize="16px" color="#42B0FF">Veja mais detalhes.</SectionText>
+              </Box>
+            </Stack>
+          </Stack>
+        </VStack>
+
       </Stack>
 
       <Stack
@@ -452,12 +410,12 @@ export default function QuemSomos({ pages }) {
             width="100%"
             spacing="100px"
           >
-              {team["nome-do-time"].map((elm, index) => (
-                <TeamBox
-                  index={index}
-                  bio={elm}
-                />
-              ))}
+            {team["nome-do-time"].map((elm, index) => (
+              <TeamBox
+                index={index}
+                bio={elm}
+              />
+            ))}
           </Stack>
         </Stack>
       </Stack>
