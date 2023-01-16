de88d8b7b270237559045d8df969d7cee14c5453
AldemirLucas <aldemirlucas23@gmail.com>
2023-01-16 16:06:44 -0300

    comment openApi

--- next/pages/openapi.js
+++ next/pages/openapi.js
@@ -1,17 +1,20 @@
-import SwaggerUI from "swagger-ui-react";
-import "swagger-ui-react/swagger-ui.css";
-import { getOpenApiSchema } from "./api/openapi";
+// import SwaggerUI from "swagger-ui-react";
+// import "swagger-ui-react/swagger-ui.css";
+// import { getOpenApiSchema } from "./api/openapi";
 
-export async function getStaticProps(context) {
-  const openApiSchema = await getOpenApiSchema();
+// export async function getStaticProps(context) {
+//   const openApiSchema = await getOpenApiSchema();
 
-  return {
-    props: {
-      openApiSchema
-    },
-  }
-}
+//   return {
+//     props: {
+//       openApiSchema
+//     },
+//   }
+// }
 
-export default function OpenApiPage({ openApiSchema }) {
-  return <SwaggerUI spec={openApiSchema} />
+// export default function OpenApiPage({ openApiSchema }) {
+//   return <SwaggerUI spec={openApiSchema} />
+// }
+export default function OpenApiPage() {
+  return <div></div>
 }
