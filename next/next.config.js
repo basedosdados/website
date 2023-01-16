eceab11aec5d4751f2566227fa49580a7108b93f
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2023-01-16 18:46:33 -0300

    modify to standalone output

--- next/next.config.js
+++ next/next.config.js
@@ -3,7 +3,10 @@ module.exports = {
     disableStaticImages: true,
     domains: [
       "basedosdados.org",
-      "basedosdados-static.s3.us-east-2.amazonaws.com",
-    ],
+      "basedosdados-static.s3.us-east-2.amazonaws.com"
+    ]
   },
+  output: "standalone",
+  reactStrictMode: true,
+  swcMinify: true
 };
