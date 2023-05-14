67306e05e4ffab9113c9f4110e5f794937d978c4
AldemirLucas <aldemirlucas23@gmail.com>
2023-05-14 19:02:02 -0300

    change getPeople and getTeam for staging

--- next/pages/api/team/getPeople.js
+++ next/pages/api/team/getPeople.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default function getPeople() {
   return axios
     .get(
-      "https://basedosdados.org/api/3/action/bd_people"
+      "https://staging.basedosdados.org/api/3/action/bd_people"
     )
     .then(({ data }) => data.result)
 }
--- next/pages/api/team/getTeams.js
+++ next/pages/api/team/getTeams.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default function getTeams() {
   return axios
     .get(
-      "https://basedosdados.org/api/3/action/bd_teams"
+      "https://staging.basedosdados.org/api/3/action/bd_teams"
     )
     .then(({ data }) => data.result)
 }
