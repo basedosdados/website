7d6950d116c5f5cdcca53c4fd17edd487ed3ca4a
AldemirLucas <aldemirlucas23@gmail.com>
2023-06-26 12:22:57 -0300

    isActive user

--- next/pages/api/user/getUser.js
+++ next/pages/api/user/getUser.js
@@ -14,6 +14,7 @@ export default async function getUser(email) {
               edges {
                 node {
                   isAdmin
+                  isActive
                   picture
                   username
                   firstName
