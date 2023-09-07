d626f9ba95e0ea3d0c5a8be5b1a521bd0561951d
Vinicius <vncsna@gmail.com>
2023-09-07 18:01:38 -0300

    feat: enable hot reload on docker compose

--- next/next.config.js
+++ next/next.config.js
@@ -8,5 +8,12 @@ module.exports = {
   },
   output: "standalone",
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  webpack: (config, context) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
