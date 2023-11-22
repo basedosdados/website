453c92e2326cfae1fbbcb3f5f17c06baa699891c
AldemirLucas <aldemirlucas23@gmail.com>
2023-11-22 14:33:05 -0300

    stripe wip

--- next/components/organisms/paymentSystem.js
+++ next/components/organisms/paymentSystem.js
@@ -1,23 +1,62 @@
+import {
+  VStack,
+  Text
+} from "@chakra-ui/react"
 import { useState, useEffect } from 'react';
 import { loadStripe} from '@stripe/stripe-js';
 import {
   Elements,
   PaymentElement,
-  CardElement,
-  BillingAddressFields,
+  useElements,
   useStripe,
-  useElements
 } from "@stripe/react-stripe-js";
+import Button from "../atoms/RoundedButton";
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-const stripePromise = loadStripe('pk_test_XKUpwPvvEnNxMsSzoLm8H3i8')
+const stripePromise = loadStripe(`${process.env.NEXT_PUBLIC_KEY_STRIPE}`)
+
+const PaymentForm = () => {
+  const stripe = useStripe()
+  const elements = useElements()
+  const [messages, setMessages] = useState("")
+
+  const handlerSubmit = async (e) => {
+    e.preventDefault()
+
+    setMessages(`${messages}<br />Submitting payment...`)
+    
+    const data = await stripe.confirmPayment({
+      elements,
+      redirect: 'if_required',
+      // confirmParams: {
+      //   return_url: "https://google.com"
+      // }
+    })
+
+    console.log(data)
+
+    if (error) {
+      setMessages(`${messages}<br />${error.message}`)
+    }
+  }
+
+  return (
+    <VStack spacing={0} alignItems="start">
+      <form onSubmit={handlerSubmit}>
+        <PaymentElement />
+
+        <Button type="submit" marginTop="20px !important">Pagar</Button>
+      </form>
+
+      {messages && <Text marginTop="20px !important">{messages}</Text>}
+    </VStack>
+  )
+}
 
 export default function PaymentSystem() {
-  const [clientSecret, setClientSecret] = useState("pi_3OAILWJxRPM20N1w0d8Rm69A_secret_Cu5T0wtbTmi9oZKtEcFeVyBnK")
+  const [clientSecret, setClientSecret] = useState("")
 
   const appearance = {
-    theme: 'stripe',
+    theme: "stripe",
   }
 
   const options = {
@@ -29,7 +68,7 @@ export default function PaymentSystem() {
     <>
       {clientSecret && (
         <Elements options={options} stripe={stripePromise}>
-          <BillingAddressFields />
+          <PaymentForm />
         </Elements>
       )}
     </>
