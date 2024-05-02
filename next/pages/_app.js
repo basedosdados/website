e38a383c1ff2b48293282209c58881cc8a379d35
Rodolfo Pinheiro <rod_bma@hotmail.com>
2024-05-02 16:12:01 -0300

    fix: add fixes on google tag manager scripts

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -13,6 +13,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
       <Head>
+        {/* <meta/> para não noindex ambientes de development e staging */}
+        <meta name="robots" content="noindex" />
+        {/* <meta/> para não noindex ambientes de development e staging */}
         <link rel="icon" type="image/ico" href="/favicon.ico" />
         <link
           rel="image_src"
@@ -56,35 +59,32 @@ function MyApp({ Component, pageProps }) {
           content="São centenas de conjuntos de dados abertos para você explorar como quiser. Baixe ou acesse dados tratados e prontos para análise usando SQL, Python, R ou Stata."
           key="ogdesc"
         />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-8WFE4K8RZX"
-        ></script>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=GTM-5NWMN98"
-        ></script>
+        {/* <!-- Google Tag Manager --> */}
         <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KC6ZJFF');`,
+            })(window,document,'script','dataLayer','GTM-5NWMN98');`,
           }}
         ></script>
-        <script
+        {/* <!-- End Google Tag Manager --> */}
+
+        {/* TAG GTM DEVELOPMENT */}
+        {/* <!-- Google Tag Manager --> */}
+        {/* <script
           dangerouslySetInnerHTML={{
-            __html: `(function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:2636848,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=JqCpoVwQlMgubUGPVq7Z9g&gtm_preview=env-10&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5NWMN98');`,
           }}
-        ></script>
+        ></script> */}
+        {/* <!-- End Google Tag Manager --> */}
+        {/* FIM DA TAG DEVELOPMENT */}
+
       </Head>
       <ChakraProvider theme={themeBD}>
         <Component {...pageProps} />
@@ -92,14 +92,28 @@ function MyApp({ Component, pageProps }) {
       {/*<!-- Google Tag Manager (noscript) -->*/}
       <noscript>
         <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-KC6ZJFF"
+          src="https://www.googletagmanager.com/ns.html?id=GTM-5NWMN98"
           height="0"
           width="0"
           style={{ display: "none", visibility: "hidden" }}
         ></iframe>
       </noscript>
       {/*<!-- End Google Tag Manager (noscript) -->*/}
-    </QueryClientProvider>
+
+      {/* TAG GTM DEVELOPMENT  */}
+      {/* <!-- Google Tag Manager (noscript) --> */}
+      {/* <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-5NWMN98&gtm_auth=JqCpoVwQlMgubUGPVq7Z9g&gtm_preview=env-10&gtm_cookies_win=x"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript> */}
+      {/* <!-- End Google Tag Manager (noscript) --> */}
+      {/* FIM DA TAG DEVELOPMENT */}
+
+    </QueryClientProvider >
   );
 }
 
