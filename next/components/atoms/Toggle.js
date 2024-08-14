913ce8b2eb2cfa355db280d7b111419316997f70
aldemirLucas <aldemirLucas23@gmail.com>
2024-08-14 19:01:25 -0300

    feat: year promotion cards att

--- next/components/atoms/Toggle.js
+++ next/components/atoms/Toggle.js
@@ -1,10 +1,10 @@
 import { Switch } from "@chakra-ui/react";
 import styles from "../../styles/toggle.module.css";
 
-export default function Toggle({ value, onChange, ...props }) {
+export default function Toggle({ value, onChange, className = "toggle", ...props }) {
   return (
     <Switch
-      className={styles.toggle}
+      className={styles[className]}
       value={value}
       onChange={onChange}
       {...props}
