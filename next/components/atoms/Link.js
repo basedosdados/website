7976d30a82f8b3b862de9309e11a66133d512a70
Jhony Lucas <jhonyjl37@gmail.com>
2024-12-12 19:00:18 -0300

    fix: ajust links external by NextLink

--- next/components/atoms/Link.js
+++ next/components/atoms/Link.js
@@ -12,6 +12,38 @@ export default function Link({
 }) {
   const { locale } = useRouter();
 
+  const isExternalLink = (() => {
+    try {
+      const linkUrl = new URL(href, window.location.origin);
+      return linkUrl.origin !== window.location.origin;
+    } catch (error) {
+      return false;
+    }
+  })();
+
+  if (isExternalLink) {
+    return (
+      <Text
+        as="a"
+        href={href}
+        target={target || "_blank"}
+        rel="noopener noreferrer"
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        fontFamily="Roboto"
+        cursor="pointer"
+        fontSize="14px"
+        lineHeight="20px"
+        fontWeight={fontWeight}
+        color={color}
+        {...props}
+      >
+        {children}
+      </Text>
+    );
+  }
+
   return (
     <NextLink href={href || '#'} locale={locale} passHref legacyBehavior>
       <Text
@@ -33,4 +65,3 @@ export default function Link({
     </NextLink>
   );
 }
-
