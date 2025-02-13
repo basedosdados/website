43a6957d78293bcceed1cdd2ecfe785d24593a55
aldemirlucas <aldemirlucas23@gmail.com>
2025-02-12 23:15:07 -0300

    feat: add member management functionality to Accesses component

--- next/components/organisms/componentsUserPage/Accesses.js
+++ next/components/organisms/componentsUserPage/Accesses.js
@@ -4,23 +4,37 @@ import {
   Image,
   Grid,
   GridItem,
-  Spinner
+  Spinner,
+  Tooltip,
+  useDisclosure,
+  ModalCloseButton
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import BodyText from "../../atoms/Text/BodyText";
+import TitleText from "../../atoms/Text/TitleText";
+import Button from "../../atoms/Button";
+
+import {
+  ModalGeneral
+} from "../../molecules/uiUserPage";
+
+import TrashIcon from "../../../public/img/icons/trashIcon";
 
 export default function Accesses ({ userInfo }) {
   const { t } = useTranslation('user');
   const [data, setData] = useState(null)
+  const AddMemberModal = useDisclosure()
+  const RemoveMemberModal = useDisclosure()
+  const [valueEmail, setValueEmail] = useState("chandra2807@uorak.com")
   const [isLoading, setIsLoading] = useState(true)
   const [owner, setOwner] = useState(null)
   const [subscriptionMembers, setSubscriptionMembers] = useState([])
 
-  async function getMembers() {
-    const reg = new RegExp("(?<=:).*")
-    const [ id ] = reg.exec(userInfo?.id)
+  const reg = new RegExp("(?<=:).*")
+  const [ id ] = reg.exec(userInfo?.id)
 
+  async function getMembers() {
     const res = await fetch(`/api/user/getMembers?p=${btoa(id)}`, {method: "GET"})
       .then(res => res.json())
     return setData(res)
@@ -43,6 +57,39 @@ export default function Accesses ({ userInfo }) {
     return setIsLoading(false)
   }, [data])
 
+  const handleAddMember = async () => {
+    setIsLoading(true)
+    const idUser = await fetch(`/api/user/getIdUser?p=${btoa(valueEmail)}`, {method: "GET"})
+      .then(res => res.json())
+    const [ idUserMember ] = reg.exec(idUser?.id)
+
+    const res = await fetch(`/api/user/addMemberInSubscription?p=${btoa(idUserMember)}&r=${btoa(id)}`, {method: "GET"})
+      .then(res => res.json())
+
+    if(res.success) {
+      getMembers()
+    } else {
+      setIsLoading(false)
+    }
+  }
+
+  const handleRemoveMember = async () => {
+    setIsLoading(true)
+    const idUser = await fetch(`/api/user/getIdUser?p=${btoa(valueEmail)}`, {method: "GET"})
+      .then(res => res.json())
+    const [ idUserMember ] = reg.exec(idUser?.id)
+
+    const res = await fetch(`/api/user/removeMemberInSubscription?p=${btoa(idUserMember)}&r=${btoa(id)}`, {method: "GET"})
+      .then(res => res.json())
+
+    if(res.success) {
+      getMembers()
+    } else {
+      setIsLoading(false)
+    }
+    setValueEmail("")
+  }
+
   const MembersComponent = ({ data, isOwner }) => {
     return (
       <>
@@ -54,16 +101,14 @@ export default function Accesses ({ userInfo }) {
           <Stack
             width="100%"
             position="relative"
-            overflow="hidden"
-            flexDir="column"
+            flexDirection="row"
             spacing={0}
             paddingLeft={{base: "16px", lg: "0"}}
-            flexDirection="row"
             alignItems={{base: "stretch", lg: "stretch"}}
+            gap="24px"
             height="54px"
           >
             <Box
-              position="absolute"
               width="48px"
               minWidth="48px"
               height="48px"
@@ -81,19 +126,24 @@ export default function Accesses ({ userInfo }) {
                 }
               />
             </Box>
-            <BodyText
-              typography="small"
-              marginLeft={{base: "44px !important", lg: "60px !important"}}
-              height="27px"
-              isTruncated
-            >{data?.firstName} {data?.lastName || ""}</BodyText>
-            <BodyText
-              typography="small"
-              marginLeft={{base: "44px !important", lg: "60px !important"}}
-              color="#464A51"
-              height="27px"
-              isTruncated
-            >{data?.email}</BodyText>
+            <Stack
+              spacing={0}
+              flexDirection="column"
+              justifyContent="center"
+              overflow="hidden"
+            >
+              <BodyText
+                typography="small"
+                height="27px"
+                isTruncated
+              >{data?.firstName} {data?.lastName || ""}</BodyText>
+              <BodyText
+                typography="small"
+                color="#464A51"
+                height="27px"
+                isTruncated
+              >{data?.email}</BodyText>
+            </Stack>
           </Stack>
         </GridItem>
 
@@ -107,13 +157,131 @@ export default function Accesses ({ userInfo }) {
           <BodyText color="#464A51" typography="small">
             {isOwner ? t('username.administrator') : t('username.member')}
           </BodyText>
+
+          {!isOwner && userInfo?.proSubscriptionRole === "owner" &&
+            <Box
+              fill="#FF8484"
+              marginLeft="auto"
+              cursor="pointer"
+              _hover={{opacity: 0.8}}
+            >
+              <Tooltip
+                label={t("username.remove")}
+                hasArrow
+                padding="16px"
+                backgroundColor="#252A32"
+                boxSizing="border-box"
+                borderRadius="8px"
+                fontFamily="Roboto"
+                fontWeight="400"
+                fontSize="14px"
+                lineHeight="20px"
+                textAlign="center"
+                color="#FFFFFF"
+                placement="top"
+                maxWidth="230px"
+              >
+                <TrashIcon
+                  width="25px"
+                  height="25px"
+                  onClick={() => {
+                    RemoveMemberModal.onOpen()
+                    setValueEmail(data?.email)
+                  }}
+                />
+              </Tooltip>
+            </Box>
+          }
         </GridItem>
       </>
     )
   }
 
   return (
-    <Stack spacing="24px" flex="1">
+    <Stack spacing="0" flex="1">
+      <ModalGeneral
+        isOpen={AddMemberModal.isOpen}
+        onClose={() => {
+          setValueEmail("")
+          AddMemberModal.onClose()
+        }}
+        propsModalContent={{
+          width: "100%",
+          maxWidth: "500px",
+          margin: "24px"
+        }}
+      >
+        <Stack spacing={0} marginBottom="40px"></Stack>
+        <Stack spacing={0} marginBottom="40px"></Stack>
+      </ModalGeneral>
+
+      <ModalGeneral
+        isOpen={RemoveMemberModal.isOpen}
+        onClose={() => {
+          RemoveMemberModal.onClose()
+          setValueEmail("")
+        }}
+        propsModalContent={{
+          width: "100%",
+          maxWidth: "500px",
+          margin: "24px"
+        }}
+      >
+        <Stack spacing={0} marginBottom="40px">
+          <TitleText marginRight="20px">
+            {t("username.modalRemoveMember")}
+          </TitleText>
+          <ModalCloseButton
+            fontSize="14px"
+            top="28px"
+            right="26px"
+            _hover={{backgroundColor: "transparent", opacity: 0.7}}
+          />
+        </Stack>
+
+        <Stack
+          display="flex"
+          flexDirection={{base: "column", lg: "row"}}
+          alignItems="center"
+          justifyContent="end"
+          gap={{base: "8px", lg: "16px"}}
+          spacing={0}
+        >
+          <Button
+            width={{base: "100%", lg: "auto"}}
+            justifyContent="center"
+            border="1px solid #BF3434"
+            color="#BF3434"
+            backgroundColor="#fff"
+            _hover={{
+              color: "#992A2A",
+              borderColor: "#992A2A"
+            }}
+            onClick={() => {
+              RemoveMemberModal.onClose()
+              setValueEmail("")
+            }}
+          >
+            {t("username.cancel")}
+          </Button>
+
+          <Button
+            width={{base: "100%", lg: "auto"}}
+            justifyContent="center"
+            backgroundColor="#BF3434"
+            _hover={{
+              backgroundColor: "#992A2A",
+            }}
+            onClick={() => {
+              RemoveMemberModal.onClose()
+              handleRemoveMember()
+            }}
+          >
+            {t("username.remove")}
+          </Button>
+        </Stack>
+      </ModalGeneral>
+
       {isLoading ?
         <Stack flex="1" justifyContent="center" marginTop="10vh">
           <Spinner
@@ -124,6 +292,16 @@ export default function Accesses ({ userInfo }) {
           />
         </Stack>
       :
+      <>
+        <Stack
+          display={userInfo?.proSubscriptionRole === "owner" ? "flex" : "none"}
+          marginBottom="24px"
+        >
+          <Button marginLeft="auto" onClick={handleAddMember}>
+            {t('username.addUser')}
+          </Button>
+        </Stack>
+
         <Grid templateColumns={{base: "1fr 1fr", lg: "3fr 1fr"}}>
           <GridItem>
             <BodyText
@@ -146,6 +324,7 @@ export default function Accesses ({ userInfo }) {
             <MembersComponent data={member?.node} isOwner={false} key={index} />
           ))}
         </Grid>
+      </>
       }
     </Stack>
   )
