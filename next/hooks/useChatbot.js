b0439877beb526e6e916104846f3aa1aa40c1160
AldemirLucas <aldemirlucas23@gmail.com>
2026-02-07 08:09:11 -0300

    chore: Remove unused `useEffect` import from `useChatbot` hook.

--- next/hooks/useChatbot.js
+++ next/hooks/useChatbot.js
@@ -1,4 +1,4 @@
-import { useState, useCallback, useRef, useEffect } from 'react';
+import { useState, useCallback, useRef } from 'react';
 import axios from 'axios';
 import cookies from 'js-cookie';
 
