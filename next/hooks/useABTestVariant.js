e89d7a384be10b4309274a7f5f8284d35b86a4bf
AldemirLucas <aldemirlucas23@gmail.com>
2026-06-02 21:28:49 -0300

    feat: add expiration parameter to useABTestVariant hook and update DatasetPage to utilize it for banner variant

--- next/hooks/useABTestVariant.js
+++ next/hooks/useABTestVariant.js
@@ -1,17 +1,21 @@
 import { useEffect, useState } from 'react';
 import cookies from 'js-cookie';
 
-export default function useABTestVariant(testId = 'default-abtest', variants = ['A', 'B']) {
+export default function useABTestVariant(
+  testId = 'default-abtest',
+  variants = ['A', 'B'],
+  expires = 365,
+) {
   const [variant, setVariant] = useState(null);
 
   useEffect(() => {
     let abVariant = cookies.get(testId);
     if (!abVariant || !variants.includes(abVariant)) {
       abVariant = variants[Math.floor(Math.random() * variants.length)];
-      cookies.set(testId, abVariant, { expires: 365 });
+      cookies.set(testId, abVariant, { expires });
     }
     setVariant(abVariant);
-  }, [testId, variants]);
+  }, [testId, variants, expires]);
 
   return variant;
 } 
