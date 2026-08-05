b10d534c41959fa569563793370512f851384e8d
Ricardo Dahis <rdahis@gmail.com>
2026-08-05 19:19:31 +1000

    fix(chatbot): show the Data Basis (DB) logo on the English site
    The chatbot sidebar and mobile header hardcoded the Base dos Dados (BD)
    wordmark, so data-basis.org showed 'BD'. Add a locale-aware BrandLogo
    (DB for en, BD otherwise), mirroring the main nav (Menu.js), and use it
    in the sidebar (expanded + collapsed) and the mobile header.
    
    (cherry picked from commit b7866bdab6f00948ab6a12f2ff0207b7048b3389)

--- next/components/organisms/chatbot/Sidebar.js
+++ next/components/organisms/chatbot/Sidebar.js
@@ -8,7 +8,7 @@ import {
   useMediaQuery,
 } from '@chakra-ui/react'
 import { useTranslation } from 'next-i18next'
-import BDLogoImage from '../../../public/img/logos/bd_logo'
+import BrandLogo from './BrandLogo'
 import { clearClientSession } from '../../../utils'
 import SidebarIcon from '../../../public/img/icons/sidebarIcon'
 import CrossIcon from '../../../public/img/icons/crossIcon'
@@ -119,11 +119,11 @@ function Sidebar({
             position="relative"
             left="-2px"
           >
-            <BDLogoImage widthImage="58px" heightImage="25px" />
+            <BrandLogo widthImage="58px" heightImage="25px" />
           </Box>
 
           {!isOpen && !isHovering && (
-            <BDLogoImage
+            <BrandLogo
               display={{ base: "none", md: "block" }}
               widthImage="34px"
               heightImage="34px"
