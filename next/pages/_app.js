4765a2474346a7f801258cd61fbaceb37187c9d1
AldemirLucas <aldemirlucas23@gmail.com>
2026-08-17 14:39:48 -0300

    feat: add Meta Pixel tracking code for analytics integration
    - Implemented Meta Pixel code for tracking page views on the production environment.
    - Added noscript fallback for Meta Pixel to ensure tracking in cases where JavaScript is disabled.

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -132,6 +132,25 @@ function MyApp({ Component, pageProps }) {
         }
         {/* <!-- End Google Tag Manager --> */}
         {/* FIM DA TAG DEVELOPMENT */}
+
+        {/* Meta Pixel Code */}
+        {local === "https://basedosdados.org" && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `!function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '773515071399019');
+              fbq('track', 'PageView');`,
+            }}
+          ></script>
+        )}
+        {/* End Meta Pixel Code */}
       </Head>
 
       <ChakraProvider>
@@ -165,6 +184,20 @@ function MyApp({ Component, pageProps }) {
       }
       {/* <!-- End Google Tag Manager (noscript) --> */}
       {/* FIM DA TAG DEVELOPMENT */}
+
+      {/* Meta Pixel Code (noscript) */}
+      {local === "https://basedosdados.org" && (
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=773515071399019&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+      )}
+      {/* End Meta Pixel Code (noscript) */}
 
     </QueryClientProvider >
   );
